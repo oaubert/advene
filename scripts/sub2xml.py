49930103b2a6536fb9e9a9bb16a987d2e5922e52
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-04-21 21:22:27 +0000

    * Updated to the new advene model API for annotation creation * We now use MillisecondFragment * Added --fps and --offset options
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@202 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- scripts/sub2xml.py
+++ scripts/sub2xml.py
@@ -5,48 +5,50 @@
 
 # Based on
 # vlc/modules/demux/util/sub.c
-# for the decoding part
+# for the subtitles decoding part
 
 import sys
 import re
+import time
 
-from advene.package import Package, Annotation
-from advene.fragment import ByteCountFragment
+import optparse
 
+from advene.model.package import Package
+from advene.model.annotation import Annotation
+from advene.model.fragment import MillisecondFragment
 
-def frame2ts (f):
+def frame2ts (f, fps=25.0, offset=0):
     """
-    Converts a frame number into a timestamp in microseconds
+    Converts a frame number into a timestamp in milliseconds, taking
+    the offset into account.
     """
-    # 4000 microseconds per frame (25 fps)
-    return  f * 4000
+    return  long(f * 1e3 / fps) + offset
 
-def ts2bc (t):
-    """Converts a timestamp into a bytecount"""
-    mux_rate = 10.08 * 1024 * 1024   # the rate we read the stream (in bytes/s)
-    ratio = long(mux_rate / 1000000)
-    return t * ratio
-
-def frame2bc (f):
-    """
-    Converts a frame number into a bytecount
-    """
-    return ts2bc (frame2ts (f))
-
-def create_annotation (package, alpha, omega, data):
+def create_annotation (package, type_, alpha, omega, data):
     """
     Creates an annotation in the package
     """
-    t = package.schemas['subtitle'].annotationTypes['annotation']
-    t.create (fragment = ByteCountFragment (begin=alpha,
-                                            end=omega),
-              contentData = data)
+    a=package.createAnnotation(
+        type=type_,
+        author="sub2xml",
+        date=time.strftime("%F"),
+        fragment=MillisecondFragment (begin=alpha, end=omega))
+    a.title=data[:20]
+    a.content.data = data
+    package.annotations.append(a)
 
-    
-def microdvd (file, p):
+def microdvd (file, p, options):
     """
-    Converts the subtitles from file and put the into the package p
+    Converts the subtitles from file and put them into the package p
     """
+    t = [ t for t in p.annotationTypes
+          if t.id == 'subtitle-annotation' ]
+    if len(t) == 1:
+        type_ = t[0]
+    else:
+        print "Error: cannot find subtitle-annotation annotation type"
+        sys.exit(1)
+
     f = open(file, "r")
     reg = re.compile (r"""{(\d+)}{(\d+)}(.*)""")
     for l in f.readlines():
@@ -58,25 +60,36 @@ def microdvd (file, p):
             end = long(end)
             sys.stdout.write('.')
             sys.stdout.flush ()
-            create_annotation (p, frame2bc (begin), frame2bc (end), unicode(data, 'iso8859-1'))
+            create_annotation (p, type_,
+                               frame2ts (begin, fps=options.fps, offset=options.offset),
+                               frame2ts (end, fps=options.fps, offset=options.offset),
+                               unicode(data, 'iso8859-1'))
         else:
             print "Incorrect line :\n%s" % l
     f.close ()
 
 def main ():
+    parser = optparse.OptionParser(usage="Usage: %prog [options] subtitles-file package-file")
+    parser.add_option("-f", "--fps",
+                      action="store", type="float", dest="fps", default=25.0,
+                      help="Specify the fps parameter")
+    parser.add_option("-o", "--offset",
+                      action="store", type="int", dest="offset", default=0,
+                      help="Specify the offset in ms")
+    (options, args) = parser.parse_args()
+    
     try:
-        subtitles_file = sys.argv[1]
-        package_file = sys.argv[2]
+        subtitles_file = args[0]
+        package_file = args[1]
     except:
-        print "Syntax: %s subtitles-file package-file" % sys.argv[0]
+        parser.print_help()
         sys.exit (1)
 
     print "Converting subtitles from %s to package %s..." % (subtitles_file,
                                                              package_file)
-    # FIXME: autodetect the format
-    
+    # FIXME: autodetect the format    
     p = Package (uri="", source="file:subtitles.xml")
-    microdvd (subtitles_file, p)
+    microdvd (subtitles_file, p, options=options)
     p.save (as=package_file)
     
 
