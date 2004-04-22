f66e6ccbdac8a40cbf008ce4bba0cbd25c0a17d1
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-04-22 18:39:34 +0000

    Splitted util.vlclib into util.vlclib and gui.util in order to remove the gtk dependency on util.vlclib
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@212 3fb6f576-06d4-0310-ad11-a4b695aa9f05

---
+++ lib/advene/gui/util.py
@@ -0,0 +1,25 @@
+"""VLC library functions."""
+
+from gettext import gettext as _
+
+import gtk
+import StringIO
+
+def image_to_pixbuf (image):
+    file = StringIO.StringIO ()
+    image.save (file, 'ppm')
+    contents = file.getvalue()
+    file.close ()
+    loader = gtk.gdk.PixbufLoader ('pnm')
+    loader.write (contents, len (contents))
+    pixbuf = loader.get_pixbuf ()
+    loader.close ()
+    return pixbuf
+
+
+def png_to_pixbuf (png_data):
+    loader = gtk.gdk.PixbufLoader ('png')
+    loader.write (png_data, len (png_data))
+    pixbuf = loader.get_pixbuf ()
+    loader.close ()
+    return pixbuf
