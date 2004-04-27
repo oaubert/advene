896d63980d8750e0d0134b535553197f991d8fd6
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-04-27 18:39:25 +0000

    Query inherits from content.WithContent
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@243 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/model/query.py
+++ lib/advene/model/query.py
@@ -3,12 +3,14 @@ import advene.model.util.uri
 import advene.model._impl as _impl
 import advene.model.modeled as modeled
 import advene.model.viewable as viewable
+import advene.model.content as content
 
 from advene.model.constants import *
 
 from advene.model.util.auto_properties import auto_properties
 
 class Query(modeled.Importable, viewable.Viewable.withClass('query'),
+            content.WithContent,
             _impl.Uried, _impl.Authored, _impl.Dated, _impl.Titled):
     """Query object offering query capabilities on the model"""
     __metaclass__ = auto_properties
