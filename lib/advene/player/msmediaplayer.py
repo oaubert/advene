a9a175611e207f69dc4770ac80ee44a708d34b19
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-05-03 21:03:30 +0000

    Update to MediaPlayer9 Put references to MSDN doc
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@323 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/player/msmediaplayer.py
+++ lib/advene/player/msmediaplayer.py
@@ -43,7 +43,7 @@ class Player:
     InitStatus=4
     EndStatus=5
     UndefinedStatus=6
-    
+
     # # Exceptions
     # PositionKeyNotSupported=VLC.PositionKeyNotSupported
     # PositionOriginNotSupported=VLC.PositionOriginNotSupported
@@ -51,17 +51,24 @@ class Player:
     # PlaylistException=VLC.PlaylistException
     # InternalException=VLC.InternalException
 
+    # List of status mappings as defined in
+    # http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wmplay/mmp_sdk/playercurrentplaylist.asp
+    statusmapping=(UndefinedStatus, EndStatus, PauseStatus, PlayingStatus,
+                   ForwardStatus, BackwardStatus, InitStatus, InitStatus,
+                   EndStatus, InitStatus, UndefinedStatus, InitStatus)
+
+    # Reference of the MediaPlayer Object Model:
+    # http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wmplay/mmp_sdk/controlreference.asp
     def __init__(self):
-        self.control=win32com.client.Dispatch("MediaPlayer.MediaPlayer.1")
-
-        # FIXME: maybe try to get the .controls attribute of the object to
-        # really control it.
+        # Old version
+        #self.player=win32com.client.Dispatch("MediaPlayer.MediaPlayer.1")
+        #self.player.AutoSize=1
+        #self.player.ShowControls=0
+        #self.player.autostart=0
         
-        self.control.AutoSize=1
-        self.control.ShowControls=0
-        self.control.autostart=0
+        # MediaPlayer9: 
+        self.player=win32com.client.Dispatch('{6BF52A52-394A-11d3-B153-00C04F79FAA6}')
 
-        # MediaPlayer9: 6BF52A52-394A-11d3-B153-00C04F79FAA6
         # MediaPlayer Core:
         #w=win32com.client.Dispatch('{09428D37-E0B9-11D2-B147-00C04F79FAA6}')
         #w.launchURL('c:\\windows\\a3dspls.wav')
@@ -71,20 +78,25 @@ class Player:
         return 0
 
     def set_media_position(self, position):
-        # FIXME
+        # FIXME: convert from Position to int.
+        # 
+        # // Seek to a frame using SMPTE time code.
+        # Player.controls.currentPositionTimecode = "[00000]01:00:30.05";
+        # current position is in s
+        self.player.currentposition=position / 1000.0
         return
     
     def start(self, position):
-        self.control.controls.Play()
+        self.player.controls.play()
 
     def pause(self, position):
-        self.control.controls.Pause()
+        self.player.controls.pause()
 
     def resume(self, position):
-        self.control.controls.Play()
+        self.player.controls.play()
 
     def stop(self, position):
-        self.control.controls.Stop()
+        self.player.controls.stop()
 
     def exit(self):
         # FIXME
@@ -92,36 +104,53 @@ class Player:
     
     def playlist_add_item(self, item):
         # FIXME: we maybe can use .URL
-        self.control.FileName=item
+        # Note: convert from dvd://dev/dvd to
+        # wmpdvd://drive/title/chapter?contentdir=path
+        # (cf http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wmplay/mmp_sdk/wmpdvdprotocol.asp)
+        self.player.url=item
         return
 
     def playlist_clear(self):
-        self.control.FileName=""
+        self.player.url=""
         return
         
     def playlist_get_list(self):
-        return [ self.control.FileName ]
+        # Could use self.player.playlist property
+        return [ self.player.url ]
 
     def snapshot(self, position):
         # FIXME
         return None
+
+    def all_snapshots(self):
+        # FIXME
+        pass
     
+    def display_text (self, message, begin, end):
+        # Use self.closedCaption object and generate SAMI captions
+        # http://msdn.microsoft.com/library/en-us/wmplay/mmp_sdk/playerclosedcaption.asp?frame=true
+        # Maybe use a UTBV to generate SAMI because we must give a SMI URL
+        
+        pass
+
     def get_stream_information(self):
         s=StreamInformation()
-        s.url=self.control.FileName
-        s.length=self.control.duration
-        s.position=self.control.????
-        s.streamstatus=statusmapping(self.control.status) ???
+        s.url=self.player.FileName
+        s.length=self.player.duration
+        s.position=self.player.currentposition
+        # For status list:
+        # http://msdn.microsoft.com/library/default.asp?url=/library/en-us/wmplay/mmp_sdk/playercurrentplaylist.asp
+        s.streamstatus=statusmapping[self.player.playstate]
         # FIXME
         pass
         
     def sound_get_volume(self):
         # FIXME: Normalize volume to be in [0..255]
-        return self.control.volume
+        return self.player.volume
 
     def sound_set_volume(self, v):
         # FIXME: Normalize volume to be in [0..255]
-        self.control.volume=v
+        self.player.volume=v
 
         
         
