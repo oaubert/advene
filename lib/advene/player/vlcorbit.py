2358c61fdc95b96e00b7394f4c7a38215cfc7be8
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-05-05 13:38:01 +0000

    We must convert the unicode filename to a string because CORBA seems to have trouble marshalling a unicode string
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@333 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/player/vlcorbit.py
+++ lib/advene/player/vlcorbit.py
@@ -1,4 +1,10 @@
-"""VLC player access.
+"""VLC player control through CORBA.
+
+Note: as the rest of the application is CORBA-agnostic, we should not
+expose anything CORBA-specific to the application. That is why the
+constants are redefined as class attributes, and the create_position
+method is provided to build the adequate Position instances.
+
 """
 
 import time
@@ -238,7 +244,7 @@ class Player(object):
             self.orb, self.mc = self.launcher.init ()
             m = self.get_default_media()
             if m is not None and m != "":
-                self.mc.playlist_add_item (m)
+                self.mc.playlist_add_item (str(m))
 
     def get_default_media (self):
         """Return the default media path (used when starting the player).
