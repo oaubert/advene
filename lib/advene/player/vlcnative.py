22723d781d0cf5faa4719e3710ce0af343b6763c
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-05-05 20:50:56 +0000

    Native python-VLC player interface
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@338 3fb6f576-06d4-0310-ad11-a4b695aa9f05

---
+++ lib/advene/player/vlcnative.py
@@ -0,0 +1,229 @@
+"""VLC access using the native python module.
+"""
+
+import time
+
+import advene.core.config as config
+
+from gettext import gettext as _
+
+
+# Alias the vlc module so that we can happily copy/paste from
+# vlcorbit.py
+import vlc as VLC
+
+class Player(object):
+    """Wrapper class for a native vlc.MediaControl object.
+
+    It provides some helper methods, and forwards other requests to
+    the VLC.MediaControl object.
+
+    @ivar mc: the VLC.MediaControl player
+    @type mc: VLC.MediaControl
+
+    @ivar relative_position: a predefined position (0-relative)
+    @type relative_position: VLC.Position
+
+    Status attributes :
+
+    @ivar current_position_value: the current player position (in ms)
+    @type current_position_value: int
+    @ivar stream_duration: the current stream duration
+    @type stream_duration: long
+    @ivar status: the player's current status
+    @type status: VLC.Status
+    """
+    # Class attributes
+    AbsolutePosition=VLC.AbsolutePosition
+    RelativePosition=VLC.RelativePosition
+    ModuloPosition=VLC.ModuloPosition
+
+    ByteCount=VLC.ByteCount
+    SampleCount=VLC.SampleCount
+    MediaTime=VLC.MediaTime
+
+    # Status
+    PlayingStatus=VLC.PlayingStatus
+    PauseStatus=VLC.PauseStatus
+    ForwardStatus=VLC.ForwardStatus
+    BackwardStatus=VLC.BackwardStatus
+    InitStatus=VLC.InitStatus
+    EndStatus=VLC.EndStatus
+    UndefinedStatus=VLC.UndefinedStatus
+    
+    # Exceptions
+    PositionKeyNotSupported=VLC.PositionKeyNotSupported
+    PositionOriginNotSupported=VLC.PositionOriginNotSupported
+    InvalidPosition=VLC.InvalidPosition
+    PlaylistException=VLC.PlaylistException
+    InternalException=VLC.InternalException
+
+    def __getattribute__ (self, name):
+        """
+        Use the defined method if necessary. Else, forward the request
+        to the mc object
+        """
+        try:
+            return object.__getattribute__ (self, name)
+        except AttributeError:
+            raise InternalException
+
+    def is_active (self):
+        """Checks whether the player is active.
+
+        @return: True if the player process is active.
+        @rtype: boolean
+        """
+        return True
+
+    def stop_player(self):
+        """Stop the player."""
+        return
+
+    def restart_player (self):
+        """Restart (cleanly) the player."""
+        return True
+
+    def exit (self):
+        self.stop_player()
+
+    def get_default_media (self):
+        """Return the default media path (used when starting the player).
+
+        This method should be overriden by the mediacontrol parent.
+        """
+        return None
+
+    def __init__ (self):
+        """Wrapper initialization.
+        """
+        # FIMXE: pass options
+        self.mc = VLC.MediaControl()
+
+        # 0 relative position
+        pos = VLC.Position ()
+        pos.origin = VLC.RelativePosition
+        pos.key = VLC.MediaTime
+        pos.value = 0
+        self.relative_position = pos
+
+        # Current position value (updated by self.position_update ())
+        self.status = VLC.UndefinedStatus
+        self.current_position_value = 0
+        self.stream_duration = 0
+
+    def update_status (self, status=None, position=None):
+        """Update the player status.
+
+        Defined status:
+           - C{start}
+           - C{pause}
+           - C{resume}
+           - C{stop}
+           - C{set}
+
+        If no status is given, it only updates the value of self.status
+
+        If C{position} is None, it will be considered as zero for the
+        "start" action, and as the current relative position for other
+        actions.
+
+        @param status: the new status
+        @type status: string
+        @param position: the position
+        @type position: VLC.Position
+        """
+        if status == "start":
+            if position is None:
+                position = VLC.Position ()
+                position.origin = VLC.AbsolutePosition
+                position.key = VLC.MediaTime
+                position.value = 0
+            elif not isinstance(position, VLC.Position):
+                p=long(position)
+                position = VLC.Position ()
+                position.origin = VLC.AbsolutePosition
+                position.key = VLC.MediaTime
+                position.value = p
+            self.check_player ()
+            self.mc.start (position)
+        else:
+            if position is None:
+                position = self.relative_position
+            elif not isinstance(position, VLC.Position):
+                p=long(position)
+                position = VLC.Position ()
+                position.origin = VLC.RelativePosition
+                position.key = VLC.MediaTime
+                position.value = p
+            if status == "pause":
+                self.check_player ()
+                if self.status == VLC.PlayingStatus:
+                    self.mc.pause (position)
+                elif self.status == VLC.PauseStatus:
+                    self.mc.resume (position)
+            elif status == "resume":
+                self.check_player()
+                self.mc.resume (position)
+            elif status == "stop":
+                self.check_player()
+                self.mc.stop (position)
+            elif status == "set":
+                self.check_player()
+                if self.status in (VLC.EndStatus, VLC.UndefinedStatus):
+                    self.mc.start (position)
+                else:
+                    self.mc.set_media_position (position)
+            elif status == "" or status == None:
+                pass
+            else:
+                print "******* Error : unknown status %s in mediacontrol.py" % status
+
+        self.position_update ()
+
+    def position_update (self):
+        """Updates the current status information."""
+        if self.mc is not None:
+            try:
+                s = self.mc.get_stream_information ()
+            except:
+                raise self.InternalException(e)
+            self.status = s.streamstatus
+            self.stream_duration = s.length
+            self.current_position_value = s.position
+            # FIXME: the returned values are wrong just after a player start
+            # (pressing Play button)
+            # Workaround for now:
+            # If the duration is larger than 24h, then set it to 0
+            if self.stream_duration > 86400000:
+                self.stream_duration = 0
+                self.current_position_value = 0
+        else:
+            self.status = VLC.UndefinedStatus
+            self.stream_duration = 0
+            self.current_position_value = 0
+
+    def create_position (self, value=0, key=None, origin=None):
+        """Create a Position.
+        
+        Returns a Position object initialized to the right value, by
+        default using a MediaTime in AbsolutePosition.
+
+        @param value: the value
+        @type value: int
+        @param key: the Position key
+        @type key: VLC.Key
+        @param origin: the Position origin
+        @type origin: VLC.Origin
+        @return: a position
+        @rtype: VLC.Position
+        """
+        if key is None:
+            key=VLC.MediaTime
+        if origin is None:
+            origin=VLC.AbsolutePosition
+        p = VLC.Position ()
+        p.origin = origin
+        p.key = key
+        p.value = value
+        return p
