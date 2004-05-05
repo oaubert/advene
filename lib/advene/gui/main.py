01d727e375254dfc3be409d8ad025de9fe0f5a0f
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-05-05 21:00:55 +0000

    Specify utf8 encoding for playlist_add_item
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@344 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/main.py
+++ lib/advene/gui/main.py
@@ -588,8 +588,8 @@ class AdveneGUI (Connect):
 
         Callback used by file_selector.
         """
-        file_ = fs.get_property ("filename")
-        self.controller.player.playlist_add_item (file_)
+        file_ = fs.get_property ("filename")        
+        self.controller.player.playlist_add_item (unicode(file_, 'utf8').encode('utf8'))
         self.controller.update_status ("start")
         return True
 
@@ -1147,7 +1147,7 @@ class AdveneGUI (Connect):
             self.controller.set_default_media(url)
             sel.get_widget().destroy()
             self.controller.player.playlist_clear()
-            self.controller.player.playlist_add_item (url)
+            self.controller.player.playlist_add_item (unicode(url,'utf8').encode('utf8'))
             self.controller.update_status ("start")
             window.destroy()
             return True
