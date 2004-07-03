93250009d772d9ac3380c1d3ed154e86e2f4176e
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-07-03 20:23:05 +0000

    * Implemented annotation recognition * Display a contextualized popup
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@627 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/transcription.py
+++ lib/advene/gui/views/transcription.py
@@ -27,40 +27,153 @@ import advene.gui.edit.create
 import advene.gui.popup
 
 class TranscriptionView:
-    def __init__ (self, controller=None, annotationtype=None, separator=" "):
+    def __init__ (self, controller=None, annotationtype=None, separator="  "):
         self.controller=controller
         self.package=controller.package
         self.model = annotationtype
         self.separator = separator
+        self.displaytime=False
+        # Annotation where the cursor is set
+        self.currentannotation=None
         self.widget=self.build_widget()
 
     def build_widget(self):
         vbox = gtk.VBox()
         self.textview = gtk.TextView()
-        # We could make it editable and modify the annotation 
-        self.textview.set_editable(False)
+        # We could make it editable and modify the annotation
+        self.textview.set_editable(True)
         self.textview.set_wrap_mode (gtk.WRAP_CHAR)
 
+        self.generate_buffer_content()
+
         b=self.textview.get_buffer()
+
+        activated_tag = b.create_tag("activated")
+        activated_tag.set_property("weight", pango.WEIGHT_BOLD)
+
+        currenttag = b.create_tag("current")
+        currenttag.set_property("background", "lightblue")
+
+        self.textview.connect("button-press-event", self.button_press_event_cb)
+        self.textview.connect_after("move-cursor", self.move_cursor_cb)
+        self.textview.connect("insert-at-cursor", self.insert_at_cursor_cb)
+        self.textview.connect("populate-popup", self.populate_popup_cb)
+
+        self.update_current_annotation(self.textview, None)
+        vbox.add(self.textview)
+        vbox.show_all()
+        return vbox
+
+    def generate_buffer_content(self):
+        b=self.textview.get_buffer()
+        # Clear the buffer
+        begin,end=b.get_bounds()
+        b.delete(begin, end)
+
         for a in self.model.annotations:
             mark = b.create_mark("b_%s" % a.id,
                                  b.get_iter_at_mark(b.get_insert()),
                                  left_gravity=True)
-            #b.insert_at_cursor("[%s]" % vlclib.format_time(a.fragment.begin))
+            mark.set_visible(True)
+            if self.displaytime:
+                b.insert_at_cursor("[%s]" % vlclib.format_time(a.fragment.begin))
             b.insert_at_cursor(a.content.data)
+            if self.displaytime:
+                b.insert_at_cursor("[%s]" % vlclib.format_time(a.fragment.end))
             mark = b.create_mark("e_%s" % a.id,
                                  b.get_iter_at_mark(b.get_insert()),
                                  left_gravity=True)
+            mark.set_visible(True)
             #print "inserted from %d to %d" % (b_a, e_a)
             b.insert_at_cursor(self.separator)
+        return
 
-        activated_tag = b.create_tag("activated")
-        activated_tag.set_property("background", "red")
-        activated_tag.set_property("weight", pango.WEIGHT_BOLD)
-        
-        vbox.add(self.textview)
-        vbox.show_all()
-        return vbox
+    def populate_popup_cb(self, textview, menu):
+        if self.currentannotation is None:
+            return False
+
+        item = gtk.MenuItem(_("Annotation %s") % self.currentannotation.id)
+        menuc=advene.gui.popup.Menu(self.currentannotation,
+                                    controller=self.controller)
+        item.set_submenu(menuc.menu)
+        item.show_all()
+        menu.append(item)
+
+        return False
+
+    def insert_at_cursor_cb(self, textview, s):
+        if self.currentannotation is None:
+            return False
+
+        b=self.textview.get_buffer()
+        b.insert_at_cursor(s)
+        # Update the annotation's content
+        beginiter=b.get_iter_at_mark(b.get_mark("b_%s" % self.currentannotation.id))
+        enditer  =b.get_iter_at_mark(b.get_mark("e_%s" % self.currentannotation.id))
+        self.currentannotation.content.data = b.get_text(beginiter, enditer)
+        print "Updated value to %s" % self.currentannotation.content.data
+        return True
+
+    def button_press_event_cb(self, textview, event):
+        self.update_current_annotation()
+        return False
+
+    def move_cursor_cb(self, textview, step_size, count, extend_selection):
+        self.update_current_annotation()
+        return False
+
+    def update_current_annotation(self, *p, **kw):
+        b=self.textview.get_buffer()
+        i=b.get_iter_at_mark(b.get_insert())
+
+        annotationid=None
+
+        # Are we on an annotation bound ?
+        marknames = [ m.get_name()
+                      for m in i.get_marks() ]
+        beginmarks= [ n
+                      for n in marknames
+                      if n and n.startswith('b_') ]
+        endmarks= [ n
+                    for n in marknames
+                    if n and n.startswith('e_') ]
+        if beginmarks or endmarks:
+            # Do not activate on annotation boundary
+            # (it causes problem when editing)
+            annotationid=None
+        else:
+            # Look backwards for the first mark that we find
+            while True:
+                marknames = [ m.get_name()
+                              for m in i.get_marks() ]
+                beginmarks= [ n
+                              for n in marknames
+                              if n and n.startswith('b_') ]
+                endmarks= [ n
+                            for n in marknames
+                            if n and n.startswith('e_') ]
+                if beginmarks:
+                    break
+                if endmarks:
+                    break
+                i.backward_char()
+
+            if beginmarks:
+                annotationid=beginmarks[0].replace('b_', '')
+
+        if annotationid is not None:
+            a=self.package.annotations['#'.join( (self.package.uri,
+                                                  annotationid) )]
+            if a != self.currentannotation:
+                if self.currentannotation is not None:
+                    self.untag_annotation(self.currentannotation, "current")
+                self.currentannotation=a
+                self.tag_annotation(a, "current")
+        else:
+            if self.currentannotation is not None:
+                self.untag_annotation(self.currentannotation, "current")
+                self.currentannotation=None
+        return False
 
     def update_annotation (self, annotation=None, event=None):
         """Update an annotation's representation."""
@@ -88,18 +201,24 @@ class TranscriptionView:
             print "Unknown event %s" % event
         return True
 
+    def tag_annotation(self, a, tagname):
+        b=self.textview.get_buffer()
+        beginiter=b.get_iter_at_mark(b.get_mark("b_%s" % a.id))
+        enditer  =b.get_iter_at_mark(b.get_mark("e_%s" % a.id))
+        b.apply_tag_by_name(tagname, beginiter, enditer)
+
+    def untag_annotation(self, a, tagname):
+        b=self.textview.get_buffer()
+        beginiter=b.get_iter_at_mark(b.get_mark("b_%s" % a.id))
+        enditer  =b.get_iter_at_mark(b.get_mark("e_%s" % a.id))
+        b.remove_tag_by_name(tagname, beginiter, enditer)
+
     def activate_annotation(self, a):
-        b=self.textview.get_buffer()
-        beginiter=b.get_iter_at_mark(b.get_mark("b_%s" % a.id))
-        enditer  =b.get_iter_at_mark(b.get_mark("e_%s" % a.id))
-        b.apply_tag_by_name("activated", beginiter, enditer)
+        self.tag_annotation(a, "activated")
         return True
-    
+
     def desactivate_annotation(self, a):
-        b=self.textview.get_buffer()
-        beginiter=b.get_iter_at_mark(b.get_mark("b_%s" % a.id))
-        enditer  =b.get_iter_at_mark(b.get_mark("e_%s" % a.id))
-        b.remove_tag_by_name("activated", beginiter, enditer)
+        self.untag_annotation(a, "activated")
         return True
 
     def register_callback (self, controller=None):
@@ -108,23 +227,23 @@ class TranscriptionView:
                                                 method=self.activate_annotation_handler)
         self.endrule=controller.event_handler.internal_rule (event="AnnotationEnd",
                                                 method=self.desactivate_annotation_handler)
-        
+
     def unregister_callback (self, controller=None):
         controller.event_handler.remove_rule(self.beginrule, type_="internal")
-        controller.event_handler.remove_rule(self.endrule, type_="internal")    
+        controller.event_handler.remove_rule(self.endrule, type_="internal")
 
     def activate_annotation_handler (self, context, parameters):
         annotation=context.evaluateValue('annotation')
         if annotation is not None and annotation.type == self.model:
             self.activate_annotation (annotation)
         return True
-            
+
     def desactivate_annotation_handler (self, context, parameters):
         annotation=context.evaluateValue('annotation')
         if annotation is not None and annotation.type == self.model:
             self.desactivate_annotation (annotation)
         return True
-    
+
     def get_widget (self):
         """Return the TreeView widget."""
         return self.widget
@@ -139,7 +258,7 @@ class TranscriptionView:
                                                        or self.model.id))
 
         vbox = gtk.VBox()
-        
+
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         vbox.add (sw)
@@ -155,9 +274,9 @@ class TranscriptionView:
         hb.add(b)
 
         vbox.pack_start(hb, expand=False)
-        
+
         window.add(vbox)
-        
+
         window.show_all()
         return window
 
@@ -165,20 +284,20 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Should provide a package name"
         sys.exit(1)
-    
+
     class DummyController:
         def notify(self, *p, **kw):
             print "Notify %s %s" % (p, kw)
 
     controller=DummyController()
-    
+
     controller.package = Package (uri=sys.argv[1])
 
     transcription = TranscriptionView(controller=controller,
                                       annotationtype=controller.package.annotationTypes[0])
-    
+
     window = transcription.popup()
-    
+
     i = 0
     def key_pressed_cb (win, event):
         global i
@@ -195,7 +314,7 @@ if __name__ == "__main__":
                 print "Activating %s" % a.content.data
                 transcription.activate_annotation(a)
                 i += 1
-        return False            
+        return False
 
     window.connect ("key-press-event", key_pressed_cb)
     window.connect ("destroy", lambda e: gtk.main_quit())
