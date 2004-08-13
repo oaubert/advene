53609506bbe661d0c7d3cccb0b7b53d4e92acde8
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-08-13 17:02:06 +0000

    Added list_selector function
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@664 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/util.py
+++ lib/advene/gui/util.py
@@ -3,7 +3,10 @@
 from gettext import gettext as _
 
 import gtk
+import gobject
 import StringIO
+
+import advene.util.vlclib as vlclib
 
 def image_to_pixbuf (image):
     file = StringIO.StringIO ()
@@ -16,10 +19,54 @@ def image_to_pixbuf (image):
     loader.close ()
     return pixbuf
 
-
 def png_to_pixbuf (png_data):
     loader = gtk.gdk.PixbufLoader ('png')
     loader.write (png_data, len (png_data))
     pixbuf = loader.get_pixbuf ()
     loader.close ()
     return pixbuf
+
+def list_selector(title=None,
+                  text=None,
+                  members=None,
+                  controller=None):
+    """Pick an element from a list.
+
+    Return None if the action is canceled.
+    """
+    store=gtk.ListStore(
+        gobject.TYPE_PYOBJECT,
+        gobject.TYPE_STRING
+        )
+    for el in members:
+        store.append( [el,
+                       vlclib.get_title(controller, el)] )
+    treeview=gtk.TreeView(store)
+    treeview.set_headers_visible(False)
+    
+    renderer = gtk.CellRendererText()
+    column = gtk.TreeViewColumn(None, renderer, text=1)
+    treeview.append_column(column)
+
+    d = gtk.Dialog(title=title,
+                   parent=None,
+                   flags=gtk.DIALOG_DESTROY_WITH_PARENT,
+                   buttons=( gtk.STOCK_OK, gtk.RESPONSE_ACCEPT,
+                             gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT ))
+
+    if text is not None:
+        l=gtk.Label(text)
+        l.show()
+        d.vbox.add(l)
+
+    d.vbox.add(treeview)
+    treeview.show()
+
+    res=d.run()
+    retval=None
+    if res == gtk.RESPONSE_ACCEPT:
+        model, iter=treeview.get_selection().get_selected()
+        if iter is not None:
+            retval=model.get_value(iter, 0)
+    d.destroy()
+    return retval
