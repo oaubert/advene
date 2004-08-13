3b2bbead7a44d1d5caa12ba8edbf103d944f6783
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-08-13 13:55:42 +0000

    Implemented treeview to edit package imports
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@650 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/edit/import.py
+++ lib/advene/gui/edit/import.py
@@ -1,61 +1,136 @@
 """GUI to import package elements.
 """
 import sys
+import pygtk
+pygtk.require('2.0')
 import gtk
 import gobject
 
 from gettext import gettext as _
 
-class HomogeneousImporter:
+import advene.gui.popup
+
+class TreeViewImporter:
     COLUMN_ELEMENT=0
     COLUMN_LABEL=1
     COLUMN_ID=2
     COLUMN_IMPORTED=3
+
+    types_mapping={'view': 'views',
+                   'schema': 'schemas',
+                   'annotation-type': 'annotationTypes',
+                   'relation-type': 'relationTypes'}
     
-    def __init__(self, controller=None, sourcepackage=None,
-                 elementtype=None):
+    def __init__(self, controller=None):
         self.controller=controller
-        self.sourcepackage=sourcepackage
-        # Name of the elementtype: views, annotationTypes,
-        # relationTypes, schemas
-        self.elementtype=elementtype
         self.widget=self.build_widget()
+
+    def tree_view_button_cb(self, widget=None, event=None):
+        retval = False
+        button = event.button
+        x = int(event.x)
+        y = int(event.y)
+        
+        if button == 3:
+            if event.window is widget.get_bin_window():
+                model = widget.get_model()
+                t = widget.get_path_at_pos(x, y)
+                if t is not None:
+                    path, col, cx, cy = t
+                    node=model[path][self.COLUMN_ELEMENT]
+                    widget.get_selection().select_path (path)
+                    menu = advene.gui.popup.Menu(node, controller=self.controller)
+                    menu.popup()
+                    retval = True
+        return retval
 
     def is_imported(self, el):
         """Check wether the element is imported in the controller's package."""
-        check=[ v.uri
-                for v in getattr(self.controller.package, self.elementtype)
-                if v.isImported() ]
-        return ( el.uri in check )
+        source=None
+        try:
+            at=self.types_mapping[el.viewableClass]
+            source=getattr(self.controller.package, at)
+        except AttributeError:
+            print "Exception on %s" % str(el)
+            return False
+        l=[ e.uri for e in source if e.isImported() ]
+        return (el.uri in l)
     
     def build_liststore(self):
         # Store reference to the element, string representation (title and id)
         # and boolean indicating wether it is imported or not
-        store=gtk.ListStore(gobject.TYPE_PYOBJECT,
+        store=gtk.TreeStore(gobject.TYPE_PYOBJECT,
                             gobject.TYPE_STRING,
                             gobject.TYPE_STRING,
                             gobject.TYPE_BOOLEAN)
-        source=getattr(self.sourcepackage, self.elementtype)        
-        for el in source:
-            store.append([el,
-                          el.title or el.id,
-                          el.id,
-                          self.is_imported(el)])
+        
+        for i in self.controller.package.imports:
+            p=i.package
+            packagerow=store.append(parent=None,
+                                    row=[p,
+                                         p.title,
+                                         i.getAs(),
+                                         True])
+            viewsrow=store.append(parent=packagerow,
+                                  row=[p.views,
+                                       _('Views'),
+                                       '',
+                                       False])
+            for v in p.views:
+                store.append(parent=viewsrow,
+                             row=[v,
+                                  v.title or v.id,
+                                  v.id,
+                                  self.is_imported(v)]) 
+
+            schemasrow=store.append(parent=packagerow,
+                                    row=[p.schemas,
+                                         _('Schemas'),
+                                         '',
+                                         False])
+            for s in p.schemas:
+                srow=store.append(parent=schemasrow,
+                                  row=[s,
+                                       #FIXME
+                                       #s.title or s.id,
+                                       #s.id,
+                                       s.title,
+                                       s.uri,
+                                       self.is_imported(s)])
+                for at in s.annotationTypes:
+                    store.append(parent=srow,
+                                 row=[at,
+                                      #FIXME
+                                      #at.title or at.id,
+                                      #at.id,
+                                      at.title,
+                                      at.uri,
+                                      self.is_imported(at)])
+                for rt in s.relationTypes:
+                    store.append(parent=srow,
+                                 row=[rt,
+                                      #FIXME
+                                      #rt.title or rt.id,
+                                      #rt.id,
+                                      rt.title,
+                                      rt.uri,
+                                      self.is_imported(rt)])
         return store
 
     def toggled_cb(self, renderer, path, model, column):
         model[path][column] = not model[path][column]
         # FIXME: should update the self.controller.package accordingly
         # But we need the corresponding methods in the model first
-        print "Toggled %s" % str(path)
+        print "Toggled %s" % model[path][self.COLUMN_LABEL]
         return False
-    
+
     def build_widget(self):
         vbox=gtk.VBox()
         
         self.store=self.build_liststore()
 
         treeview=gtk.TreeView(model=self.store)
+        treeview.connect("button_press_event", self.tree_view_button_cb)
 
         renderer = gtk.CellRendererToggle()
         renderer.set_property('activatable', True)
@@ -89,22 +164,16 @@ class Importer:
     def build_widget(self):
         vbox=gtk.VBox()
 
-        vbox.add(gtk.Label("Elements imported from %s" % self.sourcepackage.uri))
-                 
-        # Build a notebook with different elements
-        notebook=gtk.Notebook()
-        notebook.set_tab_pos(gtk.POS_TOP)
-        notebook.popup_enable()
-        notebook.set_scrollable(True)
+        vbox.pack_start(gtk.Label("Elements imported from %s" % self.sourcepackage.uri),
+                        expand=False)
+
+        scroll_win = gtk.ScrolledWindow ()
+        scroll_win.set_policy (gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        vbox.add(scroll_win)
         
-        for t in ('views', 'schemas', 'annotationTypes', 'relationTypes'):
-            hi=HomogeneousImporter(controller=self.controller,
-                                   sourcepackage=self.sourcepackage,
-                                   elementtype=t)
-            l=gtk.Label(t.capitalize())
-            notebook.append_page(hi.widget, l)
-            
-        vbox.add(notebook)
+        ti=TreeViewImporter(controller=self.controller)
+        scroll_win.add_with_viewport(ti.widget)
+        
         return vbox
 
     def popup(self):
@@ -136,8 +205,8 @@ class Importer:
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print "Should provide two package names"
+    if len(sys.argv) < 2:
+        print "Should provide a package name"
         sys.exit(1)
 
     class DummyController:
@@ -150,10 +219,7 @@ if __name__ == "__main__":
     controller.package = Package (uri=sys.argv[1])
     controller.gui=None
 
-    sourcepackage=Package(uri=sys.argv[2])
-
-    i=Importer(controller=controller,
-               sourcepackage=sourcepackage)
+    i=Importer(controller=controller)
     window=i.popup()
     
     window.connect ("destroy", lambda e: gtk.main_quit())
