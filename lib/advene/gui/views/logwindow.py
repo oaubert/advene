6cd6486c026375f853653061bae1811da0ae0f89
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-08-31 09:49:13 +0000

    Changed the display of the history Implemented goto_position and goto_url methods
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@685 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/logwindow.py
+++ lib/advene/gui/views/logwindow.py
@@ -25,9 +25,11 @@ import gobject
 import advene.rules.elements
 
 class LogWindow:
-    def __init__ (self):
+    def __init__ (self, controller=None):
         self.widget=self.build_widget()
-        # Data is a tuple list: (timestamp, position, message, widget)
+        self.controller=controller
+        self.tooltips=gtk.Tooltips()
+        # Data is a tuple list: (timestamp, position, message, url, widget)
         self.data=[]
 
     def build_widget(self):
@@ -36,7 +38,7 @@ class LogWindow:
         sw = gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         w.add(sw)
-        
+
         self.datawidget=gtk.VBox()
         sw.add_with_viewport(self.datawidget)
 
@@ -45,42 +47,65 @@ class LogWindow:
         b.connect("clicked", lambda b: self.clear_data())
         hb.pack_start(b, expand=False)
         w.pack_start(hb, expand=False)
-        
+
         w.show_all()
         return w
 
     def clear_data(self):
         """Clear the logwindow."""
         for item in self.data:
-            self.datawidget.remove(item[3])
+            self.datawidget.remove(item[4])
         self.data=[]
         return True
-    
+
     def update_display(self):
         """Regenerate the display according to the data."""
         # FIXME: remove obsolete messages
         pass
-        
-    def add_data(self, message, position):
-        l=gtk.Label()
-        l.set_justify(gtk.JUSTIFY_LEFT)
-        l.set_markup("%s : <b>%s</b>" % (vlclib.format_time(position),
-                                         message))
-        l.show()
-        self.datawidget.pack_start(l, expand=False)
-        self.data.append( (time.time(), position, message, l) )
+
+    def goto_position(self, button=None, position=None):
+        self.controller.update_status("set", position)
         return True
-    
+
+    def goto_url(self, button=None, url=None):
+        if url:
+            self.controller.open_url(url)
+        return True
+
+    def add_data(self, message, position, url=None):
+        
+        hb=gtk.HBox()
+
+        b=gtk.Button(vlclib.format_time(position))
+        b.connect("clicked", self.goto_position, position)
+        self.tooltips.set_tip(b, _("Go to the given position"))
+        hb.pack_start(b, expand=False)
+
+        b=gtk.Button(message)
+        b.connect("clicked", self.goto_url, url)
+        self.tooltips.set_tip(b, _("Go to %s") % url)
+        hb.add(b)
+
+        hb.show_all()
+        
+        self.datawidget.pack_start(hb, expand=False)
+        self.data.append( (time.time(), position, message, url, hb) )
+        return True
+
     def logMessage (self, context, parameters):
         """Log the message in a specialized window"""
         if parameters.has_key('message'):
             message=context.evaluateValue(parameters['message'])
         else:
             return False
+        if parameters.has_key('url'):
+            url=context.evaluateValue(parameters['url'])
+        else:
+            url=None
         position=context.evaluateValue('player/current_position_value')
         # We log the given annotation
-        self.add_data(message, position)
-        
+        self.add_data(message, position, url)
+
     def get_view (self):
         """Return the display widget."""
         return self.widget
@@ -95,5 +120,6 @@ class LogWindow:
             name='LogMessage',
             method=self.logMessage,
             description=_("Log a message"),
-            parameters={'message': _("Message to log.")}
+            parameters={'message': _("Message to log."),
+                        'url': _("Corresponding URL.")}
             ))
