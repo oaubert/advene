56a71fe63a610e388d677db53a43421e6cdc3430
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-08-31 09:52:54 +0000

    * Implemented entry_dialog * Cosmetics on the yes_no_cancel_popup display * Handle double click on list_selector
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@690 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/util.py
+++ lib/advene/gui/util.py
@@ -20,6 +20,8 @@ def image_to_pixbuf (image):
     return pixbuf
 
 def png_to_pixbuf (png_data):
+    """Load PNG data into a pixbuf
+    """
     loader = gtk.gdk.PixbufLoader ('png')
     loader.write (png_data, len (png_data))
     pixbuf = loader.get_pixbuf ()
@@ -34,7 +36,7 @@ def list_selector(title=None,
 
     vlclib.get_title is invoked to get a textual representation of
     the elements of members.
-    
+
     Return None if the action is canceled.
     """
     store=gtk.ListStore(
@@ -46,7 +48,7 @@ def list_selector(title=None,
                        vlclib.get_title(controller, el)] )
     treeview=gtk.TreeView(store)
     treeview.set_headers_visible(False)
-    
+
     renderer = gtk.CellRendererText()
     column = gtk.TreeViewColumn(None, renderer, text=1)
     treeview.append_column(column)
@@ -56,6 +58,14 @@ def list_selector(title=None,
                    flags=gtk.DIALOG_DESTROY_WITH_PARENT,
                    buttons=( gtk.STOCK_OK, gtk.RESPONSE_ACCEPT,
                              gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT ))
+
+    def button_cb(widget=None, event=None):
+        if event.type == gtk.gdk._2BUTTON_PRESS:
+            # Validate the activated entry on double click.
+            d.response(gtk.RESPONSE_ACCEPT)
+            return True
+        return False
+    treeview.connect("button_press_event", button_cb)
 
     if text is not None:
         l=gtk.Label(text)
@@ -78,7 +88,7 @@ def yes_no_cancel_popup(title=None,
                         text=None):
     """Build a Yes-No-Cancel popup window.
 
-    Return codes will be in (gtk.RESPONSE_YES, gtk.RESPONSE_NO, gtk.RESPONSE_CANCEL)
+    Return codes are in (gtk.RESPONSE_YES, gtk.RESPONSE_NO, gtk.RESPONSE_CANCEL)
     """
     d = gtk.Dialog(title=title,
                    parent=None,
@@ -87,10 +97,63 @@ def yes_no_cancel_popup(title=None,
                              gtk.STOCK_NO, gtk.RESPONSE_NO,
                              gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL ))
 
+    hb=gtk.HBox()
+    hb.show()
+    d.vbox.add(hb)
+    
+    i=gtk.Image()
+    i.set_from_stock(gtk.STOCK_DIALOG_QUESTION, gtk.ICON_SIZE_DIALOG)
+    i.show()
+    hb.pack_start(i, expand=False)
+    
     if text is not None:
         l=gtk.Label(text)
         l.show()
-        d.vbox.add(l)        
+        hb.add(l)
+
     retval=d.run()
     d.destroy()
     return retval
+
+
+def entry_dialog(title=None,
+                 text=None,
+                 default=""):
+    d = gtk.Dialog(title=title,
+                   parent=None,
+                   flags=gtk.DIALOG_DESTROY_WITH_PARENT,
+                   buttons=( gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                             gtk.STOCK_OK, gtk.RESPONSE_ACCEPT,
+                             ))
+    if text:
+        l=gtk.Label(text)
+        l.show()
+        d.vbox.add(l)
+
+    e=gtk.Entry()
+    e.show()
+    if default:
+        e.set_text(default)
+    
+    def keypressed_cb(widget=None, event=None):
+        if event.keyval == gtk.keysyms.Return:
+            # Validate the activated entry
+            d.response(gtk.RESPONSE_ACCEPT)
+            return True
+        return False
+    e.connect("key_press_event", keypressed_cb)
+
+    d.vbox.add(e)
+
+    res=d.run()
+    ret=None
+    if res == gtk.RESPONSE_ACCEPT:
+        try:
+            ret=e.get_text()
+        except ValueError:
+            ret=None
+    else:
+        ret=None
+
+    d.destroy()
+    return ret
