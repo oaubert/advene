5941fa5876ce2117a22b3fa02d6464e9ea5fbe97
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2004-10-01 15:27:38 +0000

    Fixed bugs in dummy.py so that it can be used
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@772 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/player/dummy.py
+++ lib/advene/player/dummy.py
@@ -44,10 +44,11 @@ class Player:
 
     def __init__(self):
         self.playlist=[]
+        self.position_update()
         pass
 
     def log(self, *p):
-        print "Dummy player: %s" % *p
+        print "Dummy player: %s" % p
         
     def get_media_position(self, origin, key):
         self.log("get_media_position")
@@ -107,3 +108,79 @@ class Player:
 
     def sound_set_volume(self, v):
         self.log("sound_set_volume %s" % str(v))
+
+    # Helper methods
+    def create_position (self, value=0, key=None, origin=None):
+        """Create a Position.
+        """
+        return value
+
+    def update_status (self, status=None, position=None):
+        """Update the player status.
+
+        Defined status:
+           - C{start}
+           - C{pause}
+           - C{resume}
+           - C{stop}
+           - C{set}
+
+        If no status is given, it only updates the value of self.status
+
+        If C{position} is None, it will be considered as zero for the
+        "start" action, and as the current relative position for other
+        actions.
+
+        @param status: the new status
+        @type status: string
+        @param position: the position
+        @type position: long
+        """
+        print "vlchttp update_status %s" % status
+        
+        if status == "start" or status == "set":
+            if position is None:
+                position=0
+            else:
+                position=long(position)
+            self.position_update()
+            if self.status in (self.EndStatus, self.UndefinedStatus):
+                self.start(position)
+            else:
+                self.set_media_position(position)
+        else:
+            if position is None:
+                position = 0
+            else:
+                position=long(position)
+
+            if status == "pause":
+                self.position_update()
+                if self.status == self.PauseStatus:
+                    self.resume (position)
+                else:
+                    self.pause(position)
+            elif status == "resume":
+                self.resume (position)
+            elif status == "stop":
+                self.stop (position)
+            elif status == "" or status == None:
+                pass
+            else:
+                print "******* Error : unknown status %s in vlchttp" % status
+        self.position_update ()
+
+    def is_active(self):
+        # FIXME: correctly implement this
+        return True
+
+    def check_player(self):
+        # FIXME: correctly implement this
+        print "check player"
+        return True
+
+    def position_update(self):
+        s = self.get_stream_information ()
+        self.status = s.streamstatus
+        self.stream_duration = s.length
+        self.current_position_value = s.position
