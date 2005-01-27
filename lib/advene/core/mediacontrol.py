161b769afe08b93fa5453401aa04791953a6fd12
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-01-27 16:29:27 +0000

    Win32 fixes: guess the VLC path from the registry. It invalid, use the config.data.path['vlc'] supplied value
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@903 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/mediacontrol.py
+++ lib/advene/core/mediacontrol.py
@@ -1,7 +1,8 @@
 """Player factory.
 """
 
-import advene.core.config as config
+import advene.core.config as config                
+import os
 
 from gettext import gettext as _
 
@@ -11,20 +12,36 @@ class PlayerFactory:
 
     def get_player(self):
         if config.data.os == 'win32':
-            #import advene.player.dummy as playermodule
-            import os
-            # Hack needed to get the vlc module running correctly
-            # (find the interfaces)
-            try:
-                os.chdir('C:\\cygwin\\home\\oaubert\\vlc-0.8.1')
-	    except:
-                pass
-            import advene.player.vlcnative as playermodule
+            # Try to determine wether VLC is installed or not
+            vlcpath=config.data.get_registry_value('Software\\VideoLAN\\VLC','Path')
+            
+            # FIXME: Hack: for local versions of VLC (development tree)
+            # You should define the correct path in advene.ini
+            if (vlcpath is None
+                and os.path.exists(os.sep.join( (config.data.path['vlc'],
+                                                 'vlc.exe') ))):
+                print "Using local version of VLC from %s" % config.data.path['vlc']
+                vlcpath = config.data.path['vlc']
+                
+            if vlcpath is None:
+                print _("VLC does not seem to be installed. Using dummy player.")
+                import advene.player.dummy as playermodule
+            else:
+                print _("Using VLC player module")
+                # Hack needed to get the vlc module running correctly
+                # (find the interfaces)
+                try:
+                    os.chdir(vlcpath)
+                except:
+                    print "Cannot cd to %s. The player certainly won't work." % vlcpath
+                import advene.player.vlcnative as playermodule
         else:
             #import advene.player.xine as playermodule
+            
+            # Should do some checks to verify it is present
             import advene.player.vlcnative as playermodule
+            
             #import advene.player.dummy as playermodule
             #import advene.player.vlcorbit as playermodule
             
         return playermodule.Player()
-
