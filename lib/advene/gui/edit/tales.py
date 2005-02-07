0d907cc2b3e818fba135a6108953628134b0ee27
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-02-07 19:14:17 +0000

    tales.py: implemented get_text, show, hide
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@959 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/edit/tales.py
+++ lib/advene/gui/edit/tales.py
@@ -17,23 +17,38 @@ class TALESEntry:
 
     def set_text(self, t):
         self.default=t
+        self.entry.set_text(t)
 
+    def get_text(self):
+        return self.entry.get_text()
+    
     def set_editable(self, b):
         self.editable=b
-            
+        self.entry.set_editable(b)
+
+    def show(self):
+        self.widget.show_all()
+        return True
+
+    def hide(self):
+        self.widget.hide()
+        return True
+        
     def build_widget(self):
         hbox=gtk.HBox()
         self.entry=gtk.Entry()
         b=gtk.Button(stock=gtk.STOCK_FIND)
         b.connect("clicked", self.browse_expression)
-        hbox.pack_start(self.entry, expand=False)
+        hbox.pack_start(self.entry, expand=True)
         hbox.pack_start(b, expand=False)
         hbox.show_all()
         return hbox
         
     def browse_expression(self, b):
         browser = advene.gui.views.browser.Browser(self.context, controller=self.controller)
-
+        
+        # FIXME: display initial value in browser
+        
         def callback(e):
             if e is not None:
                 self.entry.set_text(e)
