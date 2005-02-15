5b5e61eb985faaedd5bd85665ac9ee0dd1fe1b3d
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-02-15 15:38:59 +0000

    Cosmetic change: split get_player methods into 2 platform-dependent methods
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@1002 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/mediacontrol.py
+++ lib/advene/core/mediacontrol.py
@@ -12,42 +12,48 @@ class PlayerFactory:
 
     def get_player(self):
         if config.data.os == 'win32':
-            # Try to determine wether VLC is installed or not
-            vlcpath=config.data.get_registry_value('Software\\VideoLAN\\VLC','Path')
-            
-            # FIXME: Hack: for local versions of VLC (development tree)
-            # You should define the correct path in advene.ini
-            if (vlcpath is None
-                and os.path.exists(os.sep.join( (config.data.path['vlc'],
-                                                 'vlc.exe') ))):
-                print "Using local version of VLC from %s" % config.data.path['vlc']
-                vlcpath = config.data.path['vlc']
-                
-            if vlcpath is None:
-                print _("VLC does not seem to be installed. Using dummy player.")
-                import advene.player.dummy as playermodule
-            else:
-                print _("Using VLC player module")
-                # Hack needed to get the vlc module running correctly
-                # (find the interfaces)
-                try:
-                    os.chdir(vlcpath)
-                except:
-                    print "Cannot cd to %s. The player certainly won't work." % vlcpath
-                import advene.player.vlcnative as playermodule
+            self.get_player_win32()
         else:
-            #import advene.player.xine as playermodule
-
-            p=config.data.player['plugin']
-
-            if p == 'vlcnative':
-                # Should do some checks to verify it is present
-                import advene.player.vlcnative as playermodule
-            elif p == 'dummy':
-                import advene.player.dummy as playermodule
-            elif p == 'vlcorbit':
-                import advene.player.vlcorbit as playermodule
-            else:
-                import advene.player.dummy as playermodule
-           
+            self.get_player_unix()
         return playermodule.Player()
+
+    def get_player_win32(self):
+        # Try to determine wether VLC is installed or not
+        vlcpath=config.data.get_registry_value('Software\\VideoLAN\\VLC','Path')
+
+        # FIXME: Hack: for local versions of VLC (development tree)
+        # You should define the correct path in advene.ini
+        if (vlcpath is None
+            and os.path.exists(os.sep.join( (config.data.path['vlc'],
+                                             'vlc.exe') ))):
+            print "Using local version of VLC from %s" % config.data.path['vlc']
+            vlcpath = config.data.path['vlc']
+
+        if vlcpath is None:
+            print _("VLC does not seem to be installed. Using dummy player.")
+            import advene.player.dummy as playermodule
+        else:
+            print _("Using VLC player module")
+            # Hack needed to get the vlc module running correctly
+            # (find the interfaces)
+            try:
+                os.chdir(vlcpath)
+            except:
+                print "Cannot cd to %s. The player certainly won't work." % vlcpath
+            import advene.player.vlcnative as playermodule
+
+    def get_player_unix(self):
+        #import advene.player.xine as playermodule
+
+        p=config.data.player['plugin']
+
+        if p == 'vlcnative':
+            # Should do some checks to verify it is present
+            import advene.player.vlcnative as playermodule
+        elif p == 'dummy':
+            import advene.player.dummy as playermodule
+        elif p == 'vlcorbit':
+            import advene.player.vlcorbit as playermodule
+        else:
+            import advene.player.dummy as playermodule
+            
