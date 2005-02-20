15ca27a8fc83fc4982588fef12bf7c06b30990c9
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-02-20 21:56:52 +0000

    history: implemented append/clear methods          support for container
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@1029 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/history.py
+++ lib/advene/gui/views/history.py
@@ -10,39 +10,78 @@ from gettext import gettext as _
 import gtk
 
 class HistoryNavigation:
-    def __init__(self, controller=None, history=None):
+    def __init__(self, controller=None, history=None, container=None, vertical=True):
         self.controller=controller
         self.history=history
+        self.container=container
+        self.scrollwindow=None
+        if history is None:
+            self.history=[]
+        self.vertical=vertical
         self.widget=self.build_widget()
 
     def activate(self, widget=None, data=None, timestamp=None):
-        print "Activated %s" % timestamp
+        self.controller.update_status("set", timestamp, notify=False)
         return True
 
-    def build_widget(self):
-        hbox=gtk.HBox()
-        for t in self.history:
-            snap=self.controller.imagecache[t]
-            
-            vbox=gtk.VBox()
-            # FIXME: make Images clickable to select time            
-            i=gtk.Image()
-            i.set_from_pixbuf(advene.gui.util.png_to_pixbuf(snap))
-            # Does not work:
-            e=gtk.EventBox()
-            e.connect("button-release-event", self.activate, t)
-            e.add(i)
-            vbox.add(e)
-            l = gtk.Label(advene.util.vlclib.format_time(t))
-            vbox.add(l)
+    def append(self, position):
+        self.history.append(position)
+        self.append_repr(position)
+        return True
+        
+    def remove_widget(self, widget=None, container=None):
+        container.remove(widget)
+        return True
+    
+    def clear(self, *p):
+        del self.history[:]
+        self.widget.foreach(self.remove_widget, self.widget)
+        return True
+        
+    def append_repr(self, t):
+        snap=self.controller.imagecache[t]
 
-            hbox.add(vbox)
-        hbox.show_all()
-        return hbox
+        vbox=gtk.VBox()
+        i=gtk.Image()
+        i.set_from_pixbuf(advene.gui.util.png_to_pixbuf(snap))
+        e=gtk.EventBox()
+        e.connect("button-release-event", self.activate, t)
+        e.add(i)
+        vbox.pack_start(e, expand=False)
+        l = gtk.Label(advene.util.vlclib.format_time(t))
+        vbox.pack_start(l, expand=False)
+        
+        vbox.show_all()
+        if self.scrollwindow:
+            if self.vertical:
+                adj=self.scrollwindow.get_vadjustment()
+            else:
+                adj=self.scrollwindow.get_hadjustment()
+            adj.set_value(adj.upper)
+        self.widget.add(vbox)
+        
+    def build_widget(self):
+        if self.vertical:
+            mainbox=gtk.VBox()
+        else:
+            mainbox=gtk.HBox()
+            
+        for t in self.history:
+            self.append_repr(t)
+
+        mainbox.show_all()
+        return mainbox
 
     def popup(self):
-        w = gtk.Window (gtk.WINDOW_TOPLEVEL)
-        w.set_title (_("Navigation history"))
+        if self.container:
+            w=gtk.Frame()
+            w.set_label(_("Navigation history"))
+            vb=gtk.VBox()
+            vb.add(w)
+            self.container.add(vb)
+        else:
+            w = gtk.Window (gtk.WINDOW_TOPLEVEL)
+            w.set_title (_("Navigation history"))
 
         sw=gtk.ScrolledWindow()
         sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
@@ -50,6 +89,11 @@ class HistoryNavigation:
         w.add(sw)
         
         sw.add_with_viewport(self.widget)
+        self.scrollwindow=sw
 
-        w.show_all()
+        b=gtk.Button(stock=gtk.STOCK_CLEAR)
+        b.connect("clicked", self.clear)
+        vb.pack_start(b, expand=False)
+
+        vb.show_all()
         return True
