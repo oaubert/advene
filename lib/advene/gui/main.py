7ffd502710b113f8dffc8e35d86e2d98935aee1a
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-02-21 16:19:18 +0000

    main: user controller.get_default_url on web browser open
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/advene/trunk@1059 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/main.py
+++ lib/advene/gui/main.py
@@ -1558,7 +1558,7 @@ class AdveneGUI (Connect):
 
     def on_start_web_browser_activate (self, button=None, data=None):
         """Open a browser on current package's root."""
-        url = self.controller.server.get_url_for_alias('advene')
+        url = self.get_default_url()
         if url is not None:
             self.webbrowser.open (url)
         else:
