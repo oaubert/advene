c6f9ee47e2e387e921e1cb5c7ab828291d21203b
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2005-11-15 13:52:56 +0000

    browser.py: remove #! line
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1423 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/browser.py
+++ lib/advene/gui/views/browser.py
@@ -15,7 +15,6 @@
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-#! /usr/bin/env python
 
 # Advene browser, a la NeXT's workspace manager
 
