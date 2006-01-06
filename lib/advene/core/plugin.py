f1d76b2fe856e6c4994207fa4c2e16726a611508
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-01-06 12:13:22 +0000

    core.plugin:  * implement with imp instead of ihooks (simpler)  * add prefix parameter to avoid nameclashes in sys.module  * add _classes attribute to Plugin
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1542 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/plugin.py
+++ lib/advene/core/plugin.py
@@ -1,47 +1,56 @@
 #! /usr/bin/python
 
-"""Plugin loader
+"""Plugin loader.
+
 """
 
-import ihooks
+import imp
 import os
+import types
 
 class PluginCollection(list):
     """A collection of plugins.
 
     A L{PluginCollection} is a list of L{Plugin} instance. It must be
-    instanciated with the directory name.
+    instanciated with the directory name.  The prefix is used to
+    register the module in sys.modules (to avoid nameclashes).
     """
-    def __init__(self, directory, prefix=""):
+    def __init__(self, directory, prefix="plugins"):
         """Loads available plugins from directory.
         
         @param directory: the plugins directory
         @type directory: string (path)
         """
+	self.prefix=prefix
         for m in os.listdir(directory):
             name, ext = os.path.splitext(m)
-            if ext == '.py' and name.startswith(prefix) and not name.startswith('_'):
-                p = Plugin(directory, name)
+            if ext == '.py' and not name.startswith('_'):
+                p = Plugin(directory, name, self.prefix)
                 self.append(p)
 
 class Plugin(object):
     """A loaded Plugin.
 
+    A Plugin *must* have a name attribute.
+
     @ivar _plugin: the loaded plugin instance
     @type _plugin: module
-    @ivar filename: the source filename
-    @type filename: string (path)
+    @ivar _classes: a list of the classes implemented by module
+    @type _classes: list of classes
+    @ivar _filename: the source filename
+    @type _filename: string (path)
     """
-    def __init__(self, directory, name):
-        loader = ihooks.BasicModuleLoader()
-        info=loader.find_module_in_dir(name, directory)
-        loader.load_module(name, info)
-        importer = ihooks.BasicModuleImporter()
-        importer.set_loader(loader)
+    def __init__(self, directory, name, prefix="plugins"):
+	def get_classes():
+	    l=[ getattr(self._plugin, n) 
+		for n in dir(self._plugin) ]
+	    return [ c for c in l if isinstance(c, types.ClassType) or isinstance(c, type) ]
 
-        self._plugin = importer.import_module(name)
-        self.filename = info[1]
+	fullname = os.path.join( directory, name + '.py' )
+	self._plugin = imp.load_source('.'.join( (prefix, name) ), fullname, open(fullname) )
+        self._filename = fullname
 	self.name = self._plugin.name
+	self._classes = get_classes()
 
     def __getattribute__ (self, name):
         """Use the defined method if available. Else, forward the request to the plugin.
@@ -59,7 +68,7 @@ class Plugin(object):
         return "Plugin %s" % name
 
 if __name__ == '__main__':
-    l=PluginCollection('plugins')
+    l = PluginCollection('plugins')
     for p in l:
-        print p
+        print p, ":", ",".join([ str(c) for c in p._classes ])
 
