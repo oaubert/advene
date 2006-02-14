93fea32bcb984ecf5e8a4a20c3e5e2667ecb17f5
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-02-14 11:41:11 +0000

    core.controller: update_snapshot only if config.data.player['snapshot']
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1655 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/controller.py
+++ lib/advene/core/controller.py
@@ -314,7 +314,8 @@ class AdveneController:
 
         @return: a boolean (~desactivation)
         """
-        if not self.imagecache.is_initialized (position):
+        if (config.data.player['snapshot'] 
+	    and not self.imagecache.is_initialized (position)):
             # FIXME: only 0-relative position for the moment
             # print "Update snapshot for %d" % position
             try:
