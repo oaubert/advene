0d406b7fef0e8e62da9bb91286970bb31f8173b9
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-03-10 17:17:03 +0000

    gui.views.__init__: define a generic AdhocView class
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1768 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/__init__.py
+++ lib/advene/gui/views/__init__.py
@@ -15,3 +15,55 @@
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
+import gtk
+
+class AdhocView:
+    def __init__(self, controller=None):
+	self.view_name = "Generic adhoc view"
+	self.view_id = 'generic'
+	self.controller = controller
+	self.widget=self.build_widget()
+
+    def close(self):
+        if self.controller and self.controller.gui:
+            self.controller.gui.unregister_view (self)
+	self.widget.destroy()
+	return True
+
+    def get_widget (self):
+        """Return the widget."""
+        return self.widget
+
+    def build_widget(self):
+	return gtk.Label(self.view_name)
+
+    def popup(self):
+        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        window.set_title (self.view_name)
+
+        vbox = gtk.VBox()
+        window.add (vbox)
+
+
+	vbox.add (self.get_widget())
+
+        if self.controller and self.controller.gui:
+            self.controller.gui.register_view (self)
+            window.connect ("destroy", self.controller.gui.close_view_cb, window, self)
+            self.controller.gui.init_window_size(window, self.view_id)
+
+        window.buttonbox = gtk.HButtonBox()
+
+        b = gtk.Button(stock=gtk.STOCK_CLOSE)
+
+        if self.controller and self.controller.gui:
+            b.connect ("clicked", self.controller.gui.close_view_cb, window, self)
+        else:
+            b.connect ("clicked", lambda w: window.destroy())
+        window.buttonbox.add (b)
+
+        vbox.pack_start(window.buttonbox, expand=False)
+        
+        window.show_all()
+        
+        return window
