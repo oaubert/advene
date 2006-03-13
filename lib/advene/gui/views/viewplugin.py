1c842d4df3a2000a0f416ac57694dc8992bf0257
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-03-13 11:59:23 +0000

    gui.views.viewplugin: inherit from AdhocView
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1779 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/viewplugin.py
+++ lib/advene/gui/views/viewplugin.py
@@ -15,7 +15,10 @@
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-class ViewPlugin:
+
+from advene.gui.views import AdhocView
+
+class ViewPlugin(AdhocView):
     """
     Abstract class defining the interface of ViewPlugins.
     =====================================================
