e876a0a23c632d7156ec27a6755c6168fd83296b
brichard <brichard@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-03-20 14:12:52 +0000

    release date updated
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1823 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/version.py
+++ lib/advene/core/version.py
@@ -15,6 +15,8 @@
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
+
 version="0.20"
 date="20060320"
 major,minor=[ long(s) for s in version.split('.') ]
+
