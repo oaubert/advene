f4ab028ff0067f6b5f741fd49d5ff6c05941c680
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-03-29 11:59:06 +0000

    model.query: remove useless import
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1947 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/model/query.py
+++ lib/advene/model/query.py
@@ -1,22 +1,20 @@
 #
 # This file is part of Advene.
-# 
+#
 # Advene is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # Advene is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-import advene.model.util.uri
-
 import advene.model._impl as _impl
 import advene.model.modeled as modeled
 import advene.model.viewable as viewable
@@ -50,7 +48,7 @@ class Query(modeled.Importable, viewable.Viewable.withClass('query'),
                 # Weird use of hash() -- will this work?
                 # http://mail.python.org/pipermail/python-dev/2001-January/011794.html
                 ident = u"q" + unicode(id(self)) + unicode(time.clock()).replace('.','')
-            self.setId(ident)        
+            self.setId(ident)
 
     # dom dependant methods
 
@@ -75,6 +73,3 @@ class QueryFactory (modeled.Factory.of (Query)):
     FIXME
     """
     pass
-
-
-
