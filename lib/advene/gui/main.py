d95f0f807be2c8a5bde9e5e88ad2cba3b231eccf
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-03-31 10:58:17 +0000

    gui.main: on open package, check if the user tried to open a video file (common mistake) and display an information message
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@1993 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/main.py
+++ lib/advene/gui/main.py
@@ -1310,6 +1310,18 @@ class AdveneGUI (Connect):
                                               button=gtk.STOCK_OPEN,
                                               default_dir=d)
         if filename:
+	    name, ext = os.path.splitext(filename.lower())
+	    if ext in config.data.video_extensions:
+		advene.gui.util.message_dialog(
+		    _("The file %s looks like a video file. You should open it through the Select video file menuitem, or the eject button in the toolbar.") % filename,
+		    icon=gtk.MESSAGE_WARNING)
+		return True
+	    if not ext in ('.xml', '.azp'):
+		# Does not look like a valid package
+		if not advene.gui.util.message_dialog(
+		    _("The file %s does not look like a valid Advene package. It should have a .azp or .xml extension. Try to open anyway?") % filename,
+		    icon=gtk.MESSAGE_QUESTION):
+		    return True
             try:
                 self.controller.load_package (uri=filename)
             except Exception, e:
