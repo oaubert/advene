3f725e274f315c748174584456aac94a85468110
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-04-14 11:21:22 +0000

    model.util.uri: revert previous change, urljoin is used by importers of util.uri
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2094 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/model/util/uri.py
+++ lib/advene/model/util/uri.py
@@ -15,6 +15,8 @@
 # along with Foobar; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
+
+from urlparse import urljoin
 from xml.dom.ext.reader import BASIC_RESOLVER
 
 
