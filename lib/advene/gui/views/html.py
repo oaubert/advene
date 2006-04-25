b6c89cf964f16e617615c6aa909f1ad159cffb98
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-04-25 13:04:00 +0000

    gui.views.html: revamped the code (through mozembed_wrapper and gtkhtml_wrapper) to be cleaner
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2102 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/html.py
+++ lib/advene/gui/views/html.py
@@ -40,6 +40,125 @@ except ImportError:
 from gettext import gettext as _
 from advene.gui.views import AdhocView
 
+class gtkhtml_wrapper:
+    def __init__(self, controller=None, notify=None):
+	self.controller=controller
+	self.notify=notify
+        self.history = []
+	self.current = ""
+	self.widget = self.build_widget()
+
+    def refresh(self, *p):
+	self.set_url(self.current)
+	return True
+
+    def back(self, *p):
+        if len(self.history) <= 1:
+            self.controller.log(_("Cannot go back: first item in history"))
+        else:
+            # Current URL
+            u=self.history.pop()
+            # Previous one
+            self.set_url(self.history[-1])
+        return True
+
+    def set_url(self, url):
+        self.update_history(url)
+	d=self.component.document
+	d.clear()
+
+	u=urllib.urlopen(url)
+
+	d.open_stream(u.info().type)
+	for l in u:
+	    d.write_stream (l)
+
+	u.close()
+	d.close_stream()
+
+	self.current=url
+	if self.notify:
+	    self.notify(url=url)
+        return True
+
+    def get_url(self):
+	return self.current
+
+    def update_history(self, url):
+        if not self.history:
+            self.history.append(url)
+        elif self.history[-1] != url:
+            self.history.append(url)
+        return
+
+    def build_widget(self):
+	w=gtk.ScrolledWindow()
+	w.set_policy (gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+
+	c=gtkhtml2.View()
+	c.document=gtkhtml2.Document()
+
+	def request_url(document, url, stream):
+	    print "request url", url, stream
+	    pass
+
+	def link_clicked(document, link):
+	    u=self.get_url()
+	    if u:
+		url=urllib.basejoin(u, link)
+	    else:
+		url=link
+	    self.set_url(url)
+	    return True
+
+	c.document.connect("link-clicked", link_clicked)
+	c.document.connect("request-url", request_url)
+
+	c.get_vadjustment().set_value(0)
+	w.set_hadjustment(c.get_hadjustment())
+	w.set_vadjustment(c.get_vadjustment())
+	c.document.clear()
+	c.set_document(c.document)
+	w.add(c)
+	self.component=c
+	return w
+
+class mozembed_wrapper:
+    def __init__(self, controller=None, notify=None):
+	self.controller=controller
+	self.notify=notify
+	self.widget=self.build_widget()
+    
+    def refresh(self, *p):
+	self.component.reload(0)
+	return True
+
+    def back(self, *p):
+	self.component.go_back()
+	return True
+
+    def set_url(self, url):
+	self.component.load_url(url)
+	return True
+
+    def get_url(self):
+	return self.component.get_location()
+    
+    def build_widget(self):
+	w=gtkmozembed.MozEmbed()
+	# A profile must be initialized, cf
+	# http://www.async.com.br/faq/pygtk/index.py?req=show&file=faq19.018.htp
+	gtkmozembed.set_profile_path("/tmp", "foobar")
+
+	def update_location(c):
+	    if self.notify:
+		self.notify(url=self.get_url())
+	    return False
+
+	w.connect("location", update_location)
+	self.component=w
+	return w
+
 class HTMLView(AdhocView):
     _engine = engine
     def __init__ (self, controller=None, url=None):
@@ -48,66 +167,33 @@ class HTMLView(AdhocView):
         self.close_on_package_load = False
         self.component=None
         self.engine = engine
-        self.history = []
 
         self.controller=controller
         self.widget=self.build_widget()
         if url is not None:
             self.open_url(url)
 
+    def notify(self, url=None):
+	if url is not None:
+	    self.current_url(url)
+	return True
+
+    def open_url(self, url=None):
+	self.component.set_url(url)
+	return True
+
     def build_widget(self):
         if engine is None:
             w=gtk.Label(_("No available HTML rendering component"))
             self.component=w
         elif engine == 'mozembed':
-            w=gtkmozembed.MozEmbed()
-            # A profile must be initialized, cf
-            # http://www.async.com.br/faq/pygtk/index.py?req=show&file=faq19.018.htp
-            gtkmozembed.set_profile_path("/tmp", "foobar")
-
-	    def debug(*p):
-		print "debug", p
-		return True
-
-	    def update_location(c):
-		uri = c.get_location()
-		self.current_url(uri)
-                self.update_history(uri)
-                return False
-
-            #w.connect("link-message", debug, "link-message")
-            w.connect("location", update_location)
-            self.component=w
+	    self.component = mozembed_wrapper(controller=self.controller,
+					      notify=self.notify)
+	    w=self.component.widget
         elif engine == 'gtkhtml2':
-            w=gtk.ScrolledWindow()
-            w.set_policy (gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-
-            c=gtkhtml2.View()
-            c.document=gtkhtml2.Document()
-
-            def request_url(document, url, stream):
-                print "request url", url, stream
-                pass
-
-            def link_clicked(document, link):
-                u=self.current_url()
-                if u:
-                    url=urllib.basejoin(u, link)
-                else:
-                    url=link
-                self.open_url(url)
-                return True
-
-            c.document.connect("link-clicked", link_clicked)
-            c.document.connect("request-url", request_url)
-	    
-            c.get_vadjustment().set_value(0)
-            w.set_hadjustment(c.get_hadjustment())
-            w.set_vadjustment(c.get_vadjustment())
-            c.document.clear()
-            c.set_document(c.document)
-            w.add(c)
-            self.component=c
+	    self.component = gtkhtml_wrapper(controller=self.controller,
+					      notify=self.notify)
+	    w=self.component.widget
 
         vbox=gtk.VBox()
         vbox.add(w)
@@ -115,7 +201,7 @@ class HTMLView(AdhocView):
 	hbox = gtk.HBox()
 
         def entry_validated(e):
-            self.open_url(self.current_url())
+            self.component.set_url(self.current_url())
             return True
 
         self.url_entry=gtk.Entry()
@@ -125,18 +211,11 @@ class HTMLView(AdhocView):
         buttonbox=gtk.HButtonBox()
 
         b=gtk.Button(stock=gtk.STOCK_GO_BACK)
-        b.connect("clicked", self.history_back)
+        b.connect("clicked", self.component.back)
         buttonbox.add(b)
 
-        def refresh(b):
-	    if engine == 'mozembed':
-		self.component.reload(0)
-	    else:
-		self.open_url(self.current_url())
-            return True
-
         b=gtk.Button(stock=gtk.STOCK_REFRESH)
-        b.connect("clicked", refresh)
+        b.connect("clicked", self.component.refresh)
         buttonbox.add(b)
 
 	hbox.add(buttonbox)
@@ -149,47 +228,3 @@ class HTMLView(AdhocView):
         if url is not None:
             self.url_entry.set_text(url)
         return self.url_entry.get_text()
-
-    def history_back(self, *p):
-	if engine == 'mozembed':
-	    self.component.go_back()
-	    return True
-        if len(self.history) <= 1:
-            self.controller.log(_("Cannot go back: first item in history"))
-        else:
-            # Current URL
-            u=self.history.pop()
-            # Previous one
-            self.open_url(self.history[-1])
-        return True
-
-    def update_history(self, url):
-        if not self.history:
-            self.history.append(url)
-        elif self.history[-1] != url:
-            self.history.append(url)
-        self.current_url(url)
-        return
-
-    # FIXME: in the gtkmozembed case, we do not use open_url,
-    # so link to the right signal to update the history
-    def open_url(self, url):
-        self.update_history(url)
-        if engine is None:
-            self.component.set_text(_("No engine to render\n%s") % url)
-        elif engine == 'mozembed':
-            self.component.load_url(url)
-        elif engine == 'gtkhtml2':
-            d=self.component.document
-            d.clear()
-
-            u=urllib.urlopen(url)
-
-            d.open_stream(u.info().type)
-            for l in u:
-                d.write_stream (l)
-
-            u.close()
-            d.close_stream()
-
-        return True
