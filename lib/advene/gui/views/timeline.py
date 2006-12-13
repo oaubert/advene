8308474e2411cbfa9f0c0e98b0e3534afdc0efa8
brichard <brichard@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2006-12-13 09:34:46 +0000

    timeline.py : fixed timelina click on win32
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2498 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/timeline.py
+++ lib/advene/gui/views/timeline.py
@@ -176,7 +176,8 @@ class TimeLine(AdhocView):
         self.layout_size=(None, None)
 
         self.layout = gtk.Layout ()
-	#self.layout.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+	if config.data.os == 'win32':
+		self.layout.add_events(gtk.gdk.BUTTON_PRESS_MASK)
 	#to catch mouse clics on win32
         #self.layout.bin_window.get_colormap().alloc_color(self.colors['relations'])
         self.layout.connect('scroll_event', self.layout_scroll_cb)
