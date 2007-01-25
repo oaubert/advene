e007e6faa75486efd2578baf20df2d11c6c9c941
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-01-25 18:48:20 +0000

    model.annotation, model.util.defaultdict: define DefaultDict in its own module
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2653 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/model/annotation.py
+++ lib/advene/model/annotation.py
@@ -31,24 +31,7 @@ from constants import *
 from exception import AdveneException
 from fragment import fragmentFactory, unknownFragment
 
-# From ASPN python cookbook
-import copy
-
-class DefaultDict(dict):
-    """Dictionary with a default value for unknown keys."""
-    def __init__(self, default=None, **items):
-        dict.__init__(self, **items)
-        self.default = default
-
-    def __getitem__(self, key):
-        if key in self: 
-            return self.get(key)
-        else:
-            ## Need copy in case self.default is something like []
-            return self.setdefault(key, copy.deepcopy(self.default))
-
-    def __copy__(self):
-        return DefaultDict(self.default, **self)
+from advene.model.util.defaultdict import DefaultDict
 
 class Annotation(modeled.Importable, content.WithContent,
                  viewable.Viewable.withClass('annotation','_get_type_uri'),
