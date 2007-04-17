a8a20fd761edfb624d1950702f0f878748c7afc0
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-04-17 18:36:50 +0000

    gui.views.interactivequery:   * create a InteractiveResult class to handle result interaction   * make both classes instances of AdhocView
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2791 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/interactivequery.py
+++ lib/advene/gui/views/interactivequery.py
@@ -34,24 +34,40 @@ from advene.model.tal.context import AdveneTalesException
 from advene.gui.views.editaccumulator import EditAccumulator
 import advene.gui.util
 
+from advene.gui.views import AdhocView
+
 from advene.gui.views.timeline import TimeLine
 
 import advene.util.helper as helper
 
-class InteractiveQuery:
-    def __init__(self, here=None, controller=None, source="package/annotations"):
-
+class InteractiveQuery(AdhocView):
+    def __init__(self, here=None, controller=None, parameters=None, source="package/annotations"):
+        self.view_name = _("Interactive query")
+        self.view_id = 'interactive_query'
+        self.close_on_package_load = False
+        self.contextual_actions = (
+            #(_("Refresh"), self.refresh),
+            (_("Save view"), self.save_view),
+            )
+        self.options = {
+            'ignore-case': True,
+            }
+        self.controller=controller
         if here is None:
             here=controller.package
-
         self.here=here
         self.source=source
-        self.controller=controller
+
+        if parameters:
+            opt, arg = self.load_parameters(parameters)
+            self.options.update(opt)
+            for n, v in arg:
+                if n == 'source':
+                    self.source=v
 
         self.querycontainer, self.query = self.get_interactive_query()
 
-
-        self.window=None
+        self.widget=self.build_widget()
 
     def get_interactive_query(self):
         l=helper.get_id(self.controller.package.queries, '_interactive')
@@ -116,32 +132,120 @@ class InteractiveQuery:
                 return True
 
         # Close the search window
-        self.window.destroy()
+        self.close()
 
-        # Present choices to display the result
-        if (isinstance(res, list) or isinstance(res, tuple)
-            or isinstance(res, AbstractBundle)):
+        # Set the result attribute for the InteractiveResult view
+        self.result=res
 
-            if not res:
-                advene.gui.util.message_dialog(_("Empty list result"))
+        # And display the result in the same viewbook or window
+        self.controller.gui.open_adhoc_view('interactiveresult', destination=self._destination, query=self)
+        return True
+        
+    def cancel(self, button=None):
+        self.close()
+        return True
+
+    def build_widget(self):
+        vbox = gtk.VBox()
+
+        l=gtk.Label(_("Search for text in the content of annotations"))
+        vbox.pack_start(l, expand=False)
+
+        self.entry=gtk.Entry()
+        self.entry.connect('activate', self.validate)
+        vbox.pack_start(self.entry, expand=False)
+
+        self.ignorecase=gtk.CheckButton(_("Ignore case"))
+        self.ignorecase.set_active(True)
+        vbox.pack_start(self.ignorecase, expand=False)
+        
+        self.advanced = gtk.Expander ()
+        self.advanced.set_label (_("Expert search"))
+        self.advanced.set_expanded(False)
+
+        vbox.add(self.advanced)
+
+        self.eq=EditQuery(self.query,
+                          editable=True,
+                          controller=self.controller)
+        self.advanced.add(self.eq.widget)
+
+        hb=gtk.HButtonBox()
+
+        b=gtk.Button(stock=gtk.STOCK_OK)
+        b.connect ("clicked", self.validate)
+        hb.pack_start(b, expand=False)
+
+        b=gtk.Button(stock=gtk.STOCK_CANCEL)
+        b.connect ("clicked", self.cancel)
+        hb.pack_start(b, expand=False)
+
+        vbox.pack_start(hb, expand=False)
+
+        def handle_key_press_event(widget, event):
+            if event.keyval == gtk.keysyms.Return:
+                self.validate()
                 return True
+            elif event.keyval == gtk.keysyms.Escape:
+                self.cancel()
+                return True
+            return False
 
+        vbox.connect('key-press-event', handle_key_press_event)
+        self.entry.grab_focus()
+
+        return vbox
+
+class InteractiveResult(AdhocView):
+    """Interactive result display.
+    
+    Either we give the query (whose .result attribute will be set), or
+    we give a simple result (structure). In the first case, an option
+    will be offered to edit the query again.
+
+    FIXME: we should be able to DND action buttons to viewbooks
+    """
+    def __init__(self, controller=None, parameters=None, query=None, result=None):
+        self.view_name = _("Interactive result")
+        self.view_id = 'interactive_result'
+        self.close_on_package_load = False
+        self.contextual_actions = (
+            #(_("Refresh"), self.refresh),
+            #(_("Save view"), self.save_view),
+            )
+        self.controller=controller
+        self.query=query
+        if result is None and query is not None:
+            result=query.result
+        self.result=result
+
+        self.widget=self.build_widget()
+
+    def build_widget(self):
+        v=gtk.VBox()
+            
+        # FIXME: if self.query: edit query again
+        if self.query:
+            b=gtk.Button(_("Edit query again"))
+            b.connect('clicked', self.edit_query)
+            v.pack_start(b, expand=False)
+            
+        # Present choices to display the result
+        if not self.result:
+            v.add(gtk.Label(_("Empty result")))
+        elif (isinstance(self.result, list) or isinstance(self.result, tuple)
+            or isinstance(self.result, AbstractBundle)):
             # Check if there are annotations
-            l=[ a for a in res if isinstance(a, Annotation) ]
-            cr=len(res)
+            l=[ a for a in self.result if isinstance(a, Annotation) ]
+            cr=len(self.result)
             cl=len(l)
-
-            w=gtk.Window()
-            w.set_title(_("Interactive evaluation result"))
-            v=gtk.VBox()
-            w.add(v)
 
             if cr == cl:
                 t=_("Result is a list of %d annotations.") % cr
             else:
                 t=_("Result is a list of  %(number)d elements with %(elements)s.") % { 
                     'elements': helper.format_element_name("annotation", len(l)),
-                    'number': len(res)}
+                    'number': len(self.result)}
             v.add(gtk.Label(t))
 
             highlight_label=_("Highlight annotations")
@@ -167,27 +271,23 @@ class InteractiveQuery:
                 b.connect('clicked', toggle_highlight, l)
                 hb.add(b)
             b=gtk.Button(_("Edit elements"))
-            b.connect('clicked', lambda b: self.open_in_edit_accumulator(res))
+            b.connect('clicked', lambda b: self.open_in_edit_accumulator(self.result))
             hb.add(b)
             b=gtk.Button(_("Open in python evaluator"))
-            b.connect('clicked', lambda b: self.open_in_evaluator(res))
+            b.connect('clicked', lambda b: self.open_in_evaluator(self.result))
             hb.add(b)
-            b=gtk.Button(stock=gtk.STOCK_CLOSE)
-            b.connect('clicked', lambda b: w.destroy())
-            hb.add(b)
-            
-            w.show_all()
-            advene.gui.util.center_on_mouse(w)
         else:
-            advene.gui.util.message_dialog(_("Result:\n%s") % unicode(res))
+            v.add(gtk.Label(_("Result:\n%s") % unicode(self.result)))
+        v.show_all()
+        return v
+
+    def edit_query(self, *p):
+        
         return True
 
     def open_in_timeline(self, l):
-        t = TimeLine (l,
-                      minimum=0,
-                      controller=self.controller)
-        window=t.popup()
-        window.set_title(_("Results of _interactive query"))
+        self.controller.gui.open_adhoc_view('timeline', destination=self._destination, elements=l, minimum=0)
+        self.close()
         return True
 
     def open_in_edit_accumulator(self, l):
@@ -203,6 +303,7 @@ class InteractiveQuery:
             window=a.popup()
             window.set_title(_("Results of _interactive query"))
         
+        self.close()
         return True
 
     def open_in_evaluator(self, l):
@@ -238,77 +339,6 @@ class InteractiveQuery:
         
         w.set_title(_("Results of _interactive query"))
         ev.set_expression('result')
+        self.close()
         return True
-
-    def cancel(self, button=None):
-        self.window.destroy()
-        return True
-
-    def popup(self):
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-
-        if self.controller.gui:
-            self.controller.gui.init_window_size(window, 'interactivequery')
-
-        window.set_title (_("Query element %s") % (self.controller.get_title(self.here)))
-
-        vbox = gtk.VBox()
-
-        l=gtk.Label(_("Search for text in the content of annotations"))
-        vbox.pack_start(l, expand=False)
-
-        self.entry=gtk.Entry()
-        vbox.pack_start(self.entry, expand=False)
-
-        self.ignorecase=gtk.CheckButton(_("Ignore case"))
-        self.ignorecase.set_active(True)
-        vbox.pack_start(self.ignorecase, expand=False)
-        
-        self.advanced = gtk.Expander ()
-        self.advanced.set_label (_("Expert search"))
-        self.advanced.set_expanded(False)
-
-        vbox.add(self.advanced)
-
-        self.eq=EditQuery(self.query,
-                          editable=True,
-                          controller=self.controller)
-        self.advanced.add(self.eq.widget)
-
-        if self.controller.gui:
-            window.connect ("destroy", self.controller.gui.close_view_cb,
-                            window, self)
-
-        hb=gtk.HButtonBox()
-
-        b=gtk.Button(stock=gtk.STOCK_OK)
-        b.connect ("clicked", self.validate)
-        hb.pack_start(b, expand=False)
-
-        b=gtk.Button(stock=gtk.STOCK_CANCEL)
-        b.connect ("clicked", self.cancel)
-        hb.pack_start(b, expand=False)
-
-        vbox.pack_start(hb, expand=False)
-
-
-        def handle_key_press_event(widget, event):
-            if event.keyval == gtk.keysyms.Return:
-                self.validate()
-                return True
-            elif event.keyval == gtk.keysyms.Escape:
-                self.cancel()
-                return True
-            return False
-
-        window.connect('key-press-event', handle_key_press_event)
-
-        window.add(vbox)
-
-        window.show_all()
-        self.entry.grab_focus()
-        self.window=window
-        advene.gui.util.center_on_mouse(window)
-
-        return window
 
