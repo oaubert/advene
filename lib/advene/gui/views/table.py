1b1081fcd50631b6c1ff4428cc7cd016d4e42f3e
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-04-27 15:46:17 +0000

    gui.views.table: implemented GenericTable
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@2864 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/table.py
+++ lib/advene/gui/views/table.py
@@ -33,10 +33,10 @@ import gtk
 COLUMN_ELEMENT=0
 COLUMN_CONTENT=1
 COLUMN_TYPE=2
-COLUMN_BEGIN=3
-COLUMN_END=4
-COLUMN_DURATION=5
-COLUMN_ID=6
+COLUMN_ID=3
+COLUMN_BEGIN=4
+COLUMN_END=5
+COLUMN_DURATION=6
 COLUMN_BEGIN_FORMATTED=7
 COLUMN_END_FORMATTED=8
 
@@ -58,16 +58,16 @@ class AnnotationTable(AdhocView):
         """Build the ListStore containing the data.
         
         """
-        l=gtk.ListStore(object, str, str, long, long, long, str, str, str)
+        l=gtk.ListStore(object, str, str, str, long, long, long, str, str)
         for a in self.elements:
             if isinstance(a, Annotation):
                 l.append( (a, 
                            self.controller.get_title(a),
                            self.controller.get_title(a.type),
+                           a.id,
                            a.fragment.begin,
                            a.fragment.end,
                            a.fragment.duration,
-                           a.id,
                            helper.format_time(a.fragment.begin),
                            helper.format_time(a.fragment.end),
                            ) )
@@ -223,3 +223,130 @@ class AnnotationTable(AdhocView):
         else:
             print "Unknown target type for drop: %d" % targetType
         return True
+
+class GenericTable(AdhocView):
+    def __init__(self, controller=None, parameters=None, elements=None):
+        self.view_name = _("Generic table view")
+	self.view_id = 'generictableview'
+	self.close_on_package_load = False
+        self.contextual_actions = (
+            )
+        self.controller=controller
+        self.elements=elements
+        self.options={}
+
+        self.model=self.build_model()
+	self.widget = self.build_widget()
+
+    def build_model(self):
+        """Build the ListStore containing the data.
+        
+        Columns: element, content (title), type, id
+        """
+        l=gtk.ListStore(object, str, str, str)
+        for e in self.elements:
+            l.append( (e,
+                       self.controller.get_title(e),
+                       helper.get_type(e),
+                       e.id) )
+        return l
+
+    def build_widget(self):
+        tree_view = gtk.TreeView(self.model)
+
+        select = tree_view.get_selection()
+        # Set SELECTION_SINGLE to be able to use get_selected()
+        select.set_mode(gtk.SELECTION_SINGLE)
+
+        tree_view.connect("button_press_event", self.tree_view_button_cb)
+        tree_view.connect("row-activated", self.row_activated_cb)
+	#tree_view.set_search_column(COLUMN_CONTENT)
+
+        def search_content(model, column, key, iter):
+            if key in model.get_value(iter, COLUMN_CONTENT):
+                return False
+            return True
+
+        tree_view.set_search_equal_func(search_content)
+
+        columns={}
+        for (name, label, col) in (
+            ('title', _("Title"), COLUMN_CONTENT),
+            ('type', _("Type"), COLUMN_TYPE),
+            ('id', _("Id"), COLUMN_ID) ):
+            columns[name]=gtk.TreeViewColumn(label, gtk.CellRendererText(), text=col)
+            columns[name].set_reorderable(True)
+            columns[name].set_sort_column_id(col)
+            tree_view.append_column(columns[name])
+
+        self.model.set_sort_column_id(COLUMN_CONTENT, gtk.SORT_ASCENDING)
+
+        # Resizable columns: title, type
+        for name in ('title', 'type'):
+            columns[name].set_sizing(gtk.TREE_VIEW_COLUMN_FIXED)
+            columns[name].set_resizable(True)
+            columns[name].set_min_width(40)
+        columns['title'].set_expand(True)
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+	sw.add(tree_view)
+
+	sw.treeview = tree_view
+
+        return sw
+
+    def get_selected_node (self, tree_view):
+        """Return the currently selected node.
+
+        None if no node is selected.
+        """
+        selection = tree_view.get_selection ()
+        if not selection:
+            return None
+        store, it = selection.get_selected()
+        node = None
+        if it is not None:
+            node = tree_view.get_model().get_value (it,
+                                                    COLUMN_ELEMENT)
+        return node
+
+    def debug_cb (self, *p, **kw):
+        print "Debug cb:\n"
+        print "Parameters: %s" % str(p)
+        print "KW: %s" % str(kw)
+
+    def row_activated_cb(self, widget, path, view_column):
+        """Edit the element on Return or double click
+        """
+        node = self.get_selected_node (widget)
+        if node is not None:
+            self.controller.gui.edit_element(node)
+            return True
+        return False
+
+    def tree_view_button_cb(self, widget=None, event=None):
+        retval = False
+        button = event.button
+        x = int(event.x)
+        y = int(event.y)
+
+        if button == 3 or button == 2:
+            if event.window is widget.get_bin_window():
+                model = self.model
+                t = widget.get_path_at_pos(x, y)
+                if t is not None:
+                    path, col, cx, cy = t
+                    it = model.get_iter(path)
+                    node = model.get_value(it,
+                                           COLUMN_ELEMENT)
+                    widget.get_selection().select_path (path)
+                    if button == 3:
+                        menu = advene.gui.popup.Menu(node, controller=self.controller)
+                        menu.popup()
+                        retval = True
+                    elif button == 2:
+                        # Expand all children
+                        widget.expand_row(path, True)
+                        retval=True
+        return retval
