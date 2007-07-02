c78e2f296c9a4c7c0397b0c69cb6e1b62d60280f
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-07-02 14:52:52 +0000

    gui.popup: use "start" instead of "set" action in Goto annotation
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3151 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/popup.py
+++ lib/advene/gui/popup.py
@@ -64,7 +64,7 @@ class Menu:
         pos = c.create_position (value=ann.fragment.begin,
                                  key=c.player.MediaTime,
                                  origin=c.player.AbsolutePosition)
-        c.update_status (status="set", position=pos)
+        c.update_status (status="start", position=pos)
         c.gui.set_current_annotation(ann)
         return True
 
