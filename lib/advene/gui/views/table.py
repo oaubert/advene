77f47d3b89284becef2d2ad26b49906cbe5fbeac
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-10-29 14:04:40 +0000

    gui.views.table: implement AnnotationTable.get_elements
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3368 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/table.py
+++ lib/advene/gui/views/table.py
@@ -64,6 +64,17 @@ class AnnotationTable(AdhocView):
         self.model=self.build_model()
         self.widget = self.build_widget()
 
+    def get_elements(self):
+        """Return the list of elements in their displayed order.
+
+        If a selection is active, return only selected elements.
+        """
+        selection = self.widget.treeview.get_selection ()
+        if not selection.count_selected_rows():
+            selection.select_all()
+        store, paths=selection.get_selected_rows()
+        return [ store.get_value (store.get_iter(p), COLUMN_ELEMENT) for p in paths ]
+
     def build_model(self):
         """Build the ListStore containing the data.
         
