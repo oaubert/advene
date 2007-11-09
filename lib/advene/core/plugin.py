e89017fbbe87849061af54caa1c4553fb132ee64
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2007-11-09 12:42:50 +0000

    core.plugin: handle plugins embedded in zip files (used in package generators such as py2exe and py2app)
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3440 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/core/plugin.py
+++ lib/advene/core/plugin.py
@@ -22,6 +22,8 @@
 import imp
 import os
 import inspect
+import zipfile
+import zipimport
 
 class PluginCollection(list):
     """A collection of plugins.
@@ -38,11 +40,40 @@ class PluginCollection(list):
         """
         super(PluginCollection, self).__init__()
         self.prefix=prefix
-        for m in os.listdir(directory):
-            name, ext = os.path.splitext(m)
-            if ext == '.py' and not name.startswith('_'):
-                p = Plugin(directory, name, self.prefix)
+
+        if os.path.exists(directory):
+            it=self.standard_plugins(directory)
+        elif '.zip' in directory:
+            it=self.zip_plugins(directory)
+        else:
+            it=None
+
+        if it:
+            for d, fname in it:
+                #print "Loading ", d, fname
+                p = Plugin(d, fname, self.prefix)
                 self.append(p)
+
+    def standard_plugins(self, d):
+        for name in os.listdir(d):
+            m, ext = os.path.splitext(name)
+            if ext == '.pyc' and not name.startswith('_'):
+                yield d, name
+
+    def zip_plugins(self, d):
+        """Extract the list of plugins from a .zip file import.
+        """
+        if not '.zip/' in d:
+            yield None
+        (zipname, plugins_dir) = d.split('.zip/')
+        zipname += '.zip'
+        z=zipfile.ZipFile(zipname, 'r')
+        p=[ os.path.splitext(n)[0]
+            for n in z.namelist()
+            if n.startswith(plugins_dir) and n.endswith('.pyc')
+            and not os.path.basename(n).startswith('_') ]
+        for name in p:
+            yield zipname, name
 
 class Plugin(object):
     """A loaded Plugin.
@@ -56,7 +87,7 @@ class Plugin(object):
     @ivar _filename: the source filename
     @type _filename: string (path)
     """
-    def __init__(self, directory, name, prefix="plugins"):
+    def __init__(self, directory, fname, prefix="plugins"):
         def get_classes():
             """Return the classes defined in the module.
             """
@@ -64,8 +95,16 @@ class Plugin(object):
                 for n in dir(self._plugin) ]
             return [ c for c in l if inspect.isclass(c) ]
 
-        fullname = os.path.join( directory, name + '.py' )
-        self._plugin = imp.load_source('_'.join( (prefix, name) ), fullname, open(fullname) )
+        if directory.endswith('.zip'):
+            zi=zipimport.zipimporter(directory)
+            self._plugin=zi.load_module(fname)
+        else:
+            name, ext = os.path.splitext(fname)
+            if ext == '.py':
+                self._plugin = imp.load_source('_'.join( (prefix, name) ), fullname, open(fullname) )
+            elif ext == '.pyc':
+                self._plugin = imp.load_compiled('_'.join( (prefix, name) ), fullname, open(fullname) )
+        fullname = os.path.join( directory, fname )
         self._filename = fullname
         self.name = self._plugin.name
         self._classes = get_classes()
