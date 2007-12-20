oaubert/advene

commit 0f771e28a7d87ac6c91331fb59e406862c3d26a5

core.config, core.controller: use sort(key=) which is more efficient than the old way.
git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3732 3fb6f576-06d4-0310-ad11-a4b695aa9f05

lib/advene/core/controller.py
@@ -38,6 +38,7 @@ import StringIO
 import gobject
 import shlex
 import itertools
+import operator
 
 import advene.core.config as config
 
@@ -1614,8 +1615,8 @@ class AdveneController:
               if a.fragment.begin >= position or a.fragment.end >= position ]
         future_begins = list(l)
         future_ends = l
-        future_begins.sort(lambda a, b: cmp(a[1], b[1]))
-        future_ends.sort(lambda a, b: cmp(a[2], b[2]))
+        future_begins.sort(key=operator.itemgetter(1))
+        future_ends.sort(key=operator.itemgetter(1))
 
         #print "Position: %s" % helper.format_time(position)
         #print "Begins: %s\nEnds: %s" % ([ a[0].id for a in future_begins[:4] ],

lib/advene/core/config.py
@@ -33,6 +33,7 @@ import os
 import cPickle
 from optparse import OptionParser
 import mimetypes
+import operator
 
 class Config(object):
     """Configuration information, platform specific.
@@ -529,7 +530,7 @@ class Config(object):
         if not l:
             return None
         else:
-            l.sort(lambda a, b: cmp(b[1], a[1]))
+            l.sort(key=operator.itemgetter(1), reverse=True)
             return l[0][0]
 
     def get_homedir(self):

commit 2997e770d2b136b272ab821ac89bb23463c249c9

core.controller: fix generate_sorted_list (bug introduced by r3732)
git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3765 3fb6f576-06d4-0310-ad11-a4b695aa9f05

lib/advene/core/controller.py
@@ -1626,7 +1626,7 @@ class AdveneController:
         future_begins = list(l)
         future_ends = l
         future_begins.sort(key=operator.itemgetter(1))
-        future_ends.sort(key=operator.itemgetter(1))
+        future_ends.sort(key=operator.itemgetter(2))
 
         #print "Position: %s" % helper.format_time(position)
         #print "Begins: %s\nEnds: %s" % ([ a[0].id for a in future_begins[:4] ],