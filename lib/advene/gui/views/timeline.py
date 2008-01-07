f01675760ff36c8b0452c0b6b9078ae1da574086
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-01-07 10:27:16 +0000

    gui.views.timeline: when selecting annotations, control+DND will extend the selection, else the new selection will replace the previous one.
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3787 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/timeline.py
+++ lib/advene/gui/views/timeline.py
@@ -1836,6 +1836,10 @@ class TimeLine(AdhocView):
             
             if (abs(x2-x1) > 20 and abs(y2-y1) > 20):
                 # The cursor has been significantly moved. Consider it is a selection.
+                if not (event.state & gtk.gdk.CONTROL_MASK):
+                    # Control was not held: it is a new selection.
+                    self.unselect_all()
+                
                 for widget in self.layout.get_children():
                     if not isinstance(widget, AnnotationWidget):
                         continue
