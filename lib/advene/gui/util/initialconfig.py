82ca5cc3abd3e924b7ec629365de31809ff75c03
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-02-04 15:08:12 +0000

    advene.gui.util.initialconfig, bin/advene: on first Advene launch (i.e. if the preferences directory does not exist yet), ask some basic configuration questions
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@3899 3fb6f576-06d4-0310-ad11-a4b695aa9f05

---
+++ lib/advene/gui/util/initialconfig.py
@@ -0,0 +1,70 @@
+#
+# This file is part of Advene.
+#
+# Advene is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Advene is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+"""Initial configuration mini-application.
+"""
+
+from gettext import gettext as _
+
+import gtk
+import gobject
+import os
+
+import advene.core.config as config
+from advene.gui.edit.properties import EditWidget
+
+class Config:
+    def __init__(self):
+        self.options={}
+        for k in ('language', 'update-check'):
+            self.options[k]=config.data.preferences[k]
+        for k in ('data', 'imagecache', 'moviepath'):
+            self.options[k]=config.data.path[k]
+        self.widget=self.build_widget()
+
+    def main(self):
+        self.widget.show_all()
+        gobject.timeout_add (10, self.run_config)
+        gtk.main()
+
+    def run_config(self):
+        res=self.widget.popup()
+        if res:
+            for k in ('language', 'update-check'):
+                config.data.preferences[k]=self.options[k]
+            for k in ('data', 'imagecache', 'moviepath'):
+                config.data.path[k]=self.options[k]
+            config.data.save_preferences()
+        gtk.main_quit()
+        return
+
+    def build_widget(self):
+        ew=EditWidget(self.options.__setitem__, self.options.get)
+        ew.set_name(_("Initial Advene configuration"))
+        ew.add_label("""<b>Welcome in Advene</b>\nThis is the first time that you run Advene. Please answer some basic configuration questions. You will be able to modify these choices from the Advene interface, in the Edit/Preferences menu.""")
+        ew.add_option(_("Interface language"), 'language', _("Language used for the interface"),
+                      {
+                "English": 'C',
+                "Francais": 'fr_FR',
+                _("System default"): '',
+                })
+        ew.add_checkbox(_("Daily check for Advene updates on the Advene website"), 'update-check', _("Daily check for updates on the Advene website"))
+
+        ew.add_dir_selector(_("Standard directory for data files"), "data", _("Standard directory for data files"))
+        #ew.add_dir_selector(_("Imagecache"), "imagecache", _("Directory for storing the snapshot cache"))
+        ew.add_dir_selector(_("Movie path"), "moviepath", _("List of directories (separated by %s) to search for movie files (_ means package directory)") % os.path.pathsep)        
+        return ew
