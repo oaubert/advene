23ee17f336fd13704558fcb8591b497329ce7b27
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-05-19 10:50:42 +0000

    setup.py: fix simpletal/cherrypy directory ignore on linux
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@4383 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- setup.py
+++ setup.py
@@ -88,15 +88,16 @@ def get_packages_list():
     """
     l=[]
     def ispackage(pl, dirname, fnames):
-        if sys.platform == 'linux2' and (dirname.endswith('cherrypy') or dirname.endswith('simpletal')):
+        if sys.platform == 'linux2' and ('cherrypy' in dirname or dirname.endswith('simpletal')):
             # On linux (at least, Debian and Ubuntu), cherrypy and
             # simpletal are packaged. So do not consider them in the
             # packages list.
-            fnames=[]
+            fnames[:]=[]
         elif '__init__.py' in fnames:
             l.append(dirname)
     os.path.walk('lib', ispackage, l)
-    return [ ".".join(name.split(os.path.sep)[1:]) for name in l ]
+    res=[ ".".join(name.split(os.path.sep)[1:]) for name in l ]
+    return res
 
 def generate_data_dir(dir_, prefix="", postfix=""):
     """Return a structure suitable for datafiles from a directory.
