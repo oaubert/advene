1ea8d45f3460903867f798604856bbfb64775782
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-06-02 20:50:46 +0000

    gui.views.annotationdisplay: fix rendering of SVG content
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@4428 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/annotationdisplay.py
+++ lib/advene/gui/views/annotationdisplay.py
@@ -106,15 +106,16 @@ class AnnotationDisplay(AdhocView):
                     loader = gtk.gdk.PixbufLoader()
                 if loader is not None:
                     try:
-                        loader.write (self.annotation.content.data, len (self.annotation.content.data))
+                        loader.write (self.annotation.content.data)
                         loader.close ()
                         p = loader.get_pixbuf ()
                         width = p.get_width()
                         height = p.get_height()
                         pixbuf=png_to_pixbuf (self.controller.package.imagecache[self.annotation.fragment.begin]).scale_simple(width, height, gtk.gdk.INTERP_BILINEAR)
                         p.composite(pixbuf, 0, 0, width, height, 0, 0, 1.0, 1.0, gtk.gdk.INTERP_BILINEAR, 255)
-                    except gobject.GError:
+                    except gobject.GError, e:
                         # The PNG data was invalid.
+                        print "Invalid image data", e
                         pixbuf=gtk.gdk.pixbuf_new_from_file(config.data.advenefile( ( 'pixmaps', 'notavailable.png' ) ))
                 else:
                     pixbuf=gtk.gdk.pixbuf_new_from_file(config.data.advenefile( ( 'pixmaps', 'notavailable.png' ) ))
