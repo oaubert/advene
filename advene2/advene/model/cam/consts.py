6a3d79b8c4f93a0ea6ccd4c9acada1da981a45ad
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-10-15 14:54:33 +0000

    better namespace URI (for both XML *and* metadata)
    Original author: pchampin@liris.cnrs.fr
    Date: 2008-02-22 14:55:25+00:00
    
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5068 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- advene2/advene/model/cam/consts.py
+++ advene2/advene/model/cam/consts.py
@@ -2,7 +2,7 @@ from advene.model.consts import ADVENE_NS_PREFIX
 
 BOOTSTRAP_URI = "http://liris.cnrs.fr/advene/cam/bootstrap"
 
-CAM_NS_PREFIX = "%s%s" % (ADVENE_NS_PREFIX, "cinelab-xml/0.1#")
+CAM_NS_PREFIX = "%s%s" % (ADVENE_NS_PREFIX, "cinelab/0.1#")
 CAMSYS_NS_PREFIX = CAM_NS_PREFIX + "system-"
 CAM_XML = CAM_NS_PREFIX
 
