95ea713b303b1d85df0a3ef0263130bc71fc29f0
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-10-15 14:57:07 +0000

    Use 0 as default duration value
    Original author: olivier.aubert@liris.cnrs.fr
    Date: 2008-06-19 14:34:06+00:00
    
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5091 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- advene2/advene/model/cam/media.py
+++ advene2/advene/model/cam/media.py
@@ -6,5 +6,5 @@ from advene.model.core.media import Media as CoreMedia
 class Media(CoreMedia, CamElement):
     pass
 
-Media.make_metadata_property(DC_NS_PREFIX + "extent", "duration", default=None)
+Media.make_metadata_property(DC_NS_PREFIX + "extent", "duration", default=0)
 Media.make_metadata_property(CAM_NS_PREFIX + "uri", default=None)
