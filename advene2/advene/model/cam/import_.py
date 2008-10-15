7612be4fbcd12fb8d07efecc2919743dae8942f6
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-10-15 15:16:45 +0000

    CAM Imports should inherit CamGroupMixin
    Original author: pchampin@liris.cnrs.fr
    Date: 2008-09-26 13:38:28+00:00
    
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5269 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- advene2/advene/model/cam/import_.py
+++ advene2/advene/model/cam/import_.py
@@ -1,7 +1,8 @@
+from advene.model.cam.group import CamGroupMixin
 from advene.model.cam.element import CamElementMixin
 from advene.model.core.import_ import Import as CoreImport
 
-class Import(CoreImport, CamElementMixin):
+class Import(CoreImport, CamElementMixin, CamGroupMixin):
     pass
 
     # TODO when renaming is implemented: prevent "cam" from being renamed
