22d587b001e778bfea0e5c4a0b08965273c705ee
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-11-18 13:26:36 +0000

    gui.edit.montage: fix variable name
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5530 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/edit/montage.py
+++ lib/advene/gui/edit/montage.py
@@ -443,7 +443,7 @@ class Montage(AdhocView):
         i=gtk.Image()
         i.set_from_file(config.data.advenefile( ( 'pixmaps', 'highlight.png') ))
         b=gtk.ToggleToolButton()
-        b.set_icon_widget(b)
+        b.set_icon_widget(i)
         b.highlight=True
         b.connect('clicked', toggle_highlight)
         tb.insert(b, -1)
