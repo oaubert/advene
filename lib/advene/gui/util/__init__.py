75ddba418e0e96d9b5e7cb64fcf53ab237113d54
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-12-15 20:47:27 +0000

    gui.util.overlay_svg_as_pixbuf: if data is not SVG, wrap it into a SVG template
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5716 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/util/__init__.py
+++ lib/advene/gui/util/__init__.py
@@ -73,6 +73,15 @@ def overlay_svg_as_pixbuf(png_data, svg_data):
     
     @return: a PNG image
     """
+    if not '<svg ' in svg_data:
+        # Generate pseudo-svg with data
+        svg_data="""<svg version='1' preserveAspectRatio="xMinYMin meet" viewBox='0 0 320 200'>
+  <text x='10' y='190' fill="white" font-size="24" stroke="black" font-family="sans-serif">
+%s
+  </text>
+</svg>
+""" % svg_data
+
     try:
         loader = gtk.gdk.PixbufLoader('svg')
     except Exception, e:
