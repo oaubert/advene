5bf8a6cb97899a438b04269364aaaea7421ad2dd
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2008-12-22 15:16:14 +0000

    gui.main: display adhoc view tip when DND
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@5771 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/main.py
+++ lib/advene/gui/main.py
@@ -363,10 +363,13 @@ class AdveneGUI(object):
                 return True
             return False
 
-        def adhoc_view_drag_begin(widget, context, pixmap):
+        def adhoc_view_drag_begin(widget, context, pixmap, view_name):
             w=gtk.Window(gtk.WINDOW_POPUP)
             w.set_decorated(False)
-            w.add(gtk.image_new_from_file(config.data.advenefile( ( 'pixmaps', pixmap) )))
+            hb=gtk.HBox()
+            hb.pack_start(gtk.image_new_from_file(config.data.advenefile( ( 'pixmaps', pixmap) )), expand=False)
+            hb.pack_start(gtk.Label(view_name), expand=False)
+            w.add(hb)
             w.show_all()
             context.set_icon_widget(w, 16, 16)
             return True
@@ -452,7 +455,7 @@ class AdveneGUI(object):
             i.set_from_file(config.data.advenefile( ( 'pixmaps', pixmap) ))
             b.add(i)
             self.tooltips.set_tip(b, tip)
-            b.connect('drag-begin', adhoc_view_drag_begin, pixmap)
+            b.connect('drag-begin', adhoc_view_drag_begin, pixmap, tip)
             b.connect('drag-data-get', adhoc_view_drag_sent, name)
             b.connect('clicked', open_view_menu, name)
             b.drag_source_set(gtk.gdk.BUTTON1_MASK,
