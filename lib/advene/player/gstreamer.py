9aa227d01b591c87217c8f7b713ca6e8accc1d69
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-05-06 14:57:59 +0000

    player.gstreamer: use video_sink first, then sink, to add GhostPad
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@6454 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/player/gstreamer.py
+++ lib/advene/player/gstreamer.py
@@ -398,7 +398,7 @@ class Player:
         print "gstreamer: using", sink
 
         print "adding ghostpad for", elements[0]
-        self.video_sink.add_pad(gst.GhostPad('sink', elements[0].get_pad('sink')))
+        self.video_sink.add_pad(gst.GhostPad('sink', elements[0].get_pad('video_sink') or elements[0].get_pad('sink')))
 
         self.player.props.video_sink=self.video_sink
 
