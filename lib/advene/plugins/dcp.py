df741e147ef7b8c0d3426821b50030373a3b17fd
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-05-25 20:59:34 +0000

    plugins.dcp: DCP importer
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@6516 3fb6f576-06d4-0310-ad11-a4b695aa9f05

---
+++ lib/advene/plugins/dcp.py
@@ -0,0 +1,96 @@
+#
+# Advene: Annotate Digital Videos, Exchange on the NEt
+# Copyright (C) 2008 Olivier Aubert <olivier.aubert@liris.cnrs.fr>
+#
+# Advene is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Advene is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Advene; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+name="DCP importer"
+
+from gettext import gettext as _
+
+import re
+import csv
+import itertools
+
+import advene.util.helper as helper
+from advene.util.importer import GenericImporter
+
+def register(controller=None):
+    controller.register_importer(DCPImporter)
+    return True
+
+timestamp_re = re.compile('(\d\d):(\d\d):(\d\d):(\d\d)')
+
+class DCPImporter(GenericImporter):
+    name = _("DCP importer")
+
+    def can_handle(fname):
+        """Return a score between 0 and 100.
+
+        100 is for the best match (specific extension), 0 is for no match at all.
+        """
+        if fname.endswith('.tsv') or fname.endswith('.txt') or fname.endswith('.csv'):
+            return 80
+        return 0
+    can_handle=staticmethod(can_handle)
+
+    def process_file(self, filename, dest=None):
+        f=open(filename, 'rU')
+        if self.package is None:
+            self.init_package()
+        schema=self.create_schema('DCP')
+        rows=csv.reader(f, 'excel-tab')
+        self.labels = rows.next()
+        self.label2type = {}
+        # Generate types
+        for l in self.labels[2::2]:
+            if l:
+                name=unicode(l, 'mac_roman')
+                self.label2type[l] = self.create_annotation_type(schema, helper.title2id(name), title=name)
+        self.convert(self.iterator(rows))
+        self.progress(1.0)
+        return self.package
+
+    def str2time(self, s):
+        m=timestamp_re.match(s)
+        if m:
+            (h, m, s, f) = m.groups()
+            t=( ((long(h) * 60 + long(m)) * 60) + long(s) ) * 1000 + long(f) * 40
+        else:
+            t=0
+        return t
+
+    def iterator(self, rows):
+        progress=0.02
+        cache={}
+        for row in rows:
+            self.progress(progress)
+            progress += .01
+            t=self.str2time(row[1])
+            for (label, tc, value) in itertools.izip(self.labels[2::2], row[2::2], row[3::2]):
+                if tc == 'IN':
+                    # Store into cache
+                    cache[label]=( t, unicode(value, 'mac_roman') )
+                elif tc == 'OUT':
+                    (begin, content) = cache.get(label, (0, 'FIXME'))
+                    yield {
+                        'begin': begin,
+                        'end': t,
+                        'content': content,
+                        'type': self.label2type[label]
+                        }
+        self.progress(1.0)
+
