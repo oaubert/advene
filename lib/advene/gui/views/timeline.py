430a6b5835ad7df34c28b0981863446b5c5ac6f1
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-05-28 22:28:41 +0000

    gui.views.timeline: adjust annotation bound, through shortcut a (begin time) / A (end time)
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@6544 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/timeline.py
+++ lib/advene/gui/views/timeline.py
@@ -917,6 +917,23 @@ class TimeLine(AdhocView):
         print ("Lower: %.1f\tUpper: %.1f\tValue: %.1f\tPage size: %.1f"
                % (a.lower, a.upper, a.value, a.page_size))
 
+    def adjust_annotation_bound(self, annotation, bound='begin'):
+        """Display a dialog to adjust the annotation bound.
+        """
+        translation={
+            'begin': _('begin'),
+            'end': _('end'),
+            }
+        t=getattr(annotation.fragment, bound)
+        new=self.controller.gui.bound_validation(t, _("Update %(bound)s of %(annotation)s") % { 'bound': translation[bound],
+                                                                                                'annotation': self.controller.get_title(annotation) })
+        if new != t:
+            self.controller.notify('EditSessionStart', element=annotation, immediate=True)
+            setattr(annotation.fragment, bound, new)
+            self.controller.notify('AnnotationEditEnd', annotation=annotation)
+            self.controller.notify('EditSessionEnd', element=annotation)
+        return True
+
     def align_annotations(self, source, dest, mode):
         new={
             'begin': source.fragment.begin,
@@ -1724,6 +1741,10 @@ class TimeLine(AdhocView):
             # Control-return: split at current player position
             self.controller.split_annotation(annotation, self.controller.player.current_position_value)
             return True
+        elif event.keyval == gtk.keysyms.a:
+            self.adjust_annotation_bound(annotation, 'begin')
+        elif event.keyval == gtk.keysyms.A:
+            self.adjust_annotation_bound(annotation, 'end')
         elif event.keyval == gtk.keysyms.p:
             # Play
             f=self.annotation_fraction(widget)
