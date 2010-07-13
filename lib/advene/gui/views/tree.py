df9a6e9e9421dedb6bfa51963904a475fa3d572a
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-07-13 15:26:32 +0000

    gui.views.tree: do not truncate annotation content at 50 characters
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@6746 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/views/tree.py
+++ lib/advene/gui/views/tree.py
@@ -197,8 +197,8 @@ class AdveneTreeModel(gtk.GenericTreeModel, gtk.TreeDragSource, gtk.TreeDragDest
             except AttributeError:
                 pass
         # FIXME: bad hardcoded value
-        if len(title) > 50:
-            title=title[:50]
+        #if len(title) > 50:
+        #    title=title[:50]
         if isinstance(node, Annotation):
             title="%s (%s, %s)" % (title,
                                    helper.format_time(node.fragment.begin),
