b9392893c48900f35c415dc233883e24c2f96d54
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2010-08-31 08:55:39 +0000

    util.helper: fix docstring
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@6819 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/util/helper.py
+++ lib/advene/util/helper.py
@@ -16,7 +16,7 @@
 # along with Advene; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
-"""VLC library functions."""
+"""Generic helper functions."""
 
 import time
 import StringIO
