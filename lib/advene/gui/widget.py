0a3ff2bbe580a0b6e6eeb4a9ac6ffb2a2f1f4751
oaubert <oaubert@3fb6f576-06d4-0310-ad11-a4b695aa9f05>
2011-09-15 14:31:42 +0000

    gui.widget: use the gui.main.save_snapshot_as method
    git-svn-id: svn+ssh://svn.gna.org/svn/advene/trunk@7438 3fb6f576-06d4-0310-ad11-a4b695aa9f05

--- lib/advene/gui/widget.py
+++ lib/advene/gui/widget.py
@@ -52,11 +52,10 @@ except ImportError:
 import advene.core.config as config
 from advene.core.imagecache import ImageCache
 
-from advene.gui.util import png_to_pixbuf, enable_drag_source, name2color, get_target_types
-from advene.gui.util import encode_drop_parameters, get_color_style
+from advene.gui.util import png_to_pixbuf, enable_drag_source, name2color
+from advene.gui.util import get_color_style
 import advene.util.helper as helper
 from advene.model.annotation import Annotation
-import advene.gui.util.dialog as dialog
 import advene.gui.popup
 
 active_color=name2color('#fdfd4b')
@@ -1054,24 +1053,7 @@ class TimestampRepresentation(gtk.Button):
             return True
 
         def save_as(it):
-            name = "%s-%010d.png" % (
-                os.path.splitext(
-                    os.path.basename(
-                        self.controller.get_default_media()))[0],
-                self.value)
-            fn = dialog.get_filename(title=_("Save screenshot to..."),
-                 action=gtk.FILE_CHOOSER_ACTION_SAVE,
-                 button=gtk.STOCK_SAVE,
-                 default_file=name)
-            if fn is not None:
-                try:
-                    f = open(fn, 'wb')
-                    f.write(str(self.controller.package.imagecache.get(self.value, epsilon=self.epsilon)))
-                    f.close()
-                    self.log(_("Screenshot saved to %s") % fn)
-                except (IOError, OSError), e:
-                    dialog.message_dialog(_("Could not save screenshot:\n %s") % unicode(e),
-                                          icon=gtk.MESSAGE_ERROR)
+            self.controller.gui.save_snapshot_as(self.value)
             return True
 
         item = gtk.MenuItem(_("Play"))
