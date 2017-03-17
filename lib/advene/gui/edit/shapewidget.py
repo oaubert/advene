9cef109fdbea8f28b93b4a4cf2a6c0d6b4c7955e
Olivier Aubert <contact@olivieraubert.net>
2017-03-17 10:51:02 +0100

    gui.edit.shapewidget: fix warnings

--- lib/advene/gui/edit/shapewidget.py
+++ lib/advene/gui/edit/shapewidget.py
@@ -39,6 +39,9 @@ FIXME: find a way to pass the background path
 
 import os
 import sys
+import gi
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
 from gi.repository import Gtk
