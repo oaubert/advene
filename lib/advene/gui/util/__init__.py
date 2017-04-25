ed13c8998282fa9074e47a02d23d617a51c00acb
Olivier Aubert <contact@olivieraubert.net>
2017-04-25 23:51:47 +0200

    gui.util: remove unused import

--- lib/advene/gui/util/__init__.py
+++ lib/advene/gui/util/__init__.py
@@ -27,7 +27,6 @@ from gi.repository import Gdk
 from gi.repository import GdkPixbuf
 from gi.repository import Gtk
 from gi.repository import GObject
-from gi.repository import cairo
 
 import urlparse
 import urllib
