b67d800815d02707d914550c02655e336c491337
Olivier Aubert <contact@olivieraubert.net>
2017-06-17 21:48:02 +0200

    cherrypy: update local copy from 3.1.2 to 3.5.0

--- lib/cherrypy/_cpwsgi_server.py
+++ lib/cherrypy/_cpwsgi_server.py
@@ -1,55 +1,70 @@
 """WSGI server interface (see PEP 333). This adds some CP-specific bits to
 the framework-agnostic wsgiserver package.
 """
+import sys
 
 import cherrypy
 from cherrypy import wsgiserver
 
 
-class CPHTTPRequest(wsgiserver.HTTPRequest):
-    
-    def __init__(self, sendall, environ, wsgi_app):
-        s = cherrypy.server
-        self.max_request_header_size = s.max_request_header_size or 0
-        self.max_request_body_size = s.max_request_body_size or 0
-        wsgiserver.HTTPRequest.__init__(self, sendall, environ, wsgi_app)
-
-
-class CPHTTPConnection(wsgiserver.HTTPConnection):
-    
-    RequestHandlerClass = CPHTTPRequest
-
-
 class CPWSGIServer(wsgiserver.CherryPyWSGIServer):
+
     """Wrapper for wsgiserver.CherryPyWSGIServer.
-    
+
     wsgiserver has been designed to not reference CherryPy in any way,
     so that it can be used in other frameworks and applications. Therefore,
     we wrap it here, so we can set our own mount points from cherrypy.tree
     and apply some attributes from config -> cherrypy.server -> wsgiserver.
     """
-    
-    ConnectionClass = CPHTTPConnection
-    
-    def __init__(self):
-        server = cherrypy.server
-        sockFile = server.socket_file
-        if sockFile:
-            bind_addr = sockFile
-        else:
-            bind_addr = (server.socket_host, server.socket_port)
-        
-        s = wsgiserver.CherryPyWSGIServer
-        s.__init__(self, bind_addr, cherrypy.tree,
-                   server.thread_pool,
-                   server.socket_host,
-                   max = server.thread_pool_max,
-                   request_queue_size = server.socket_queue_size,
-                   timeout = server.socket_timeout,
-                   shutdown_timeout = server.shutdown_timeout,
-                   )
-        self.protocol = server.protocol_version
-        self.nodelay = server.nodelay
-        self.ssl_certificate = server.ssl_certificate
-        self.ssl_private_key = server.ssl_private_key
 
+    def __init__(self, server_adapter=cherrypy.server):
+        self.server_adapter = server_adapter
+        self.max_request_header_size = (
+            self.server_adapter.max_request_header_size or 0
+        )
+        self.max_request_body_size = (
+            self.server_adapter.max_request_body_size or 0
+        )
+
+        server_name = (self.server_adapter.socket_host or
+                       self.server_adapter.socket_file or
+                       None)
+
+        self.wsgi_version = self.server_adapter.wsgi_version
+        s = wsgiserver.CherryPyWSGIServer
+        s.__init__(self, server_adapter.bind_addr, cherrypy.tree,
+                   self.server_adapter.thread_pool,
+                   server_name,
+                   max=self.server_adapter.thread_pool_max,
+                   request_queue_size=self.server_adapter.socket_queue_size,
+                   timeout=self.server_adapter.socket_timeout,
+                   shutdown_timeout=self.server_adapter.shutdown_timeout,
+                   accepted_queue_size=self.server_adapter.accepted_queue_size,
+                   accepted_queue_timeout=self.server_adapter.accepted_queue_timeout,
+                   )
+        self.protocol = self.server_adapter.protocol_version
+        self.nodelay = self.server_adapter.nodelay
+
+        if sys.version_info >= (3, 0):
+            ssl_module = self.server_adapter.ssl_module or 'builtin'
+        else:
+            ssl_module = self.server_adapter.ssl_module or 'pyopenssl'
+        if self.server_adapter.ssl_context:
+            adapter_class = wsgiserver.get_ssl_adapter_class(ssl_module)
+            self.ssl_adapter = adapter_class(
+                self.server_adapter.ssl_certificate,
+                self.server_adapter.ssl_private_key,
+                self.server_adapter.ssl_certificate_chain)
+            self.ssl_adapter.context = self.server_adapter.ssl_context
+        elif self.server_adapter.ssl_certificate:
+            adapter_class = wsgiserver.get_ssl_adapter_class(ssl_module)
+            self.ssl_adapter = adapter_class(
+                self.server_adapter.ssl_certificate,
+                self.server_adapter.ssl_private_key,
+                self.server_adapter.ssl_certificate_chain)
+
+        self.stats['Enabled'] = getattr(
+            self.server_adapter, 'statistics', False)
+
+    def error_log(self, msg="", level=20, traceback=False):
+        cherrypy.engine.log(msg, level, traceback)
