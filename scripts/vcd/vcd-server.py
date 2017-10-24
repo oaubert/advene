b6a1b57824dbd7c1af6006789fbc2db2c50246f2
Christian Hentschel <christian@chentschel.de>
2017-10-24 14:01:39 +0200

    python3 new server protocol allows model selection allows upload of multiple annotations

--- scripts/vcd/vcd-server.py
+++ scripts/vcd/vcd-server.py
@@ -3,13 +3,10 @@
 import logging
 logger = logging.getLogger(__name__)
 
-import BaseHTTPServer
+import http.server
 import json
-<<<<<<< HEAD
 import urllib.parse
-=======
-import urlparse
->>>>>>> 617043c6... reverted changes for clean pull request
+import urllib.parse
 import os
 
 from keras.applications.resnet50 import ResNet50
@@ -26,12 +23,30 @@ PORT_NUMBER = 9000
 
 CACHE_DIR = "/var/vcd/cache"
 
-models = {
-	'resnet50': {
-		'model': ResNet50(weights='imagenet'),
-		'target_size': (224,224)
-	}
+models = [
+    {
+        "id": "standard",           #FIXME: improve selection of default model
+        "label": "ResNet50",
+        "image_size": 224
+    },
+    {
+        "id": "resnet50",
+        "label": "ResNet50",
+        "image_size": 224
+    },
+]
+
+model_impls = {
+    "standard": {                   #FIXME: s.a.
+        "class":ResNet50,
+        "params": {'weights':'imagenet',},
+    },
+    "resnet50": {
+        "class":ResNet50,
+        "params": {'weights':'imagenet',},
+    }
 }
+
 top_n_preds = 3
 
 #create cachedir
@@ -52,16 +67,7 @@ class RESTHandler(http.server.BaseHTTPRequestHandler):
             "capabilities": {
                 "minimum_batch_size": 1, # # of frames
                 "maximum_batch_size": 500, # # of frames
-                "available_models": [ {
-                    "id": "standard", # id of the model
-                    "label": "Standard detection", # user-readable label for the model
-                    "image_size": 224 # width/height for squared images
-                }, {
-                    "id": "dummy", # id of the model
-                    "label": "Dummy detection", # user-readable label for the model
-                    "image_size": 512 # width/height for squared images
-                }
-                ]
+                "available_models": models
             }
         }}, s.wfile)
 
@@ -70,39 +76,71 @@ class RESTHandler(http.server.BaseHTTPRequestHandler):
         body = s.rfile.read(length).decode('utf-8')
         if s.headers['Content-type'] == 'application/json':
             post_data = json.loads(body)
-            logger.debug(body)
         else:
-            post_data = urlparse.parse_qs(body)
+            post_data = urllib.parse.parse_qs(body)
 
-        target_size
-        batch_x = np.zeros((len(post_data['frames']),target_size[0],target_size[1],3), dtype=np.float32)
-        for i,frame in enumerate(post_data['frames']):
-            # Load image to PIL format
-            img = Image.open(BytesIO(base64.b64decode(frame['screenshot'])))
-            # cache frame - FIXME: currently there is no mean to identify the video - same timstamp will overwrite an old frame (hash?)
-            img.save(os.path.join(CACHE_DIR,'{0}.png'.format(frame['timecode'])))
-            if img.mode != 'RGB':
-                img = img.convert('RGB')
-            # ResNet50 expects images at 224x224 scale:
-            hw_tuple = (target_size[1], target_size[0])
-            if img.size != hw_tuple:
-                img = img.resize(hw_tuple)
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            x = preprocess_input(x)
-            batch_x[i] = x[0,:,:,:]
-        preds = model.predict_on_batch(np.asarray(batch_x))
+        modelid = post_data['model']
+        try:    
+            model = model_impls[modelid]['class'](**model_impls[modelid]['params'])
+        except Exception as e:
+            logger.error("Unable to load model: {reason}".format(reason=e.message))
+            s.send_response(300)
+            s.send_header("Content-type", "application/json")
+            s.end_headers()
+            json.dump({
+                    "status": 300,
+                    "message": e.message,
+                 }, s.wfile)
+            return
+        
+        target_size = (dict([(m["id"],m['image_size']) for m in models]))[modelid]
+        concepts = []
+        for annotation in post_data['annotations']:
+            aid = annotation['annotationid']
+            begin = annotation['begin']
+            begin = annotation['end']
+            
+            batch_x = np.zeros((len(annotation['frames']),target_size,target_size,3), dtype=np.float32)
+            for i,frame in enumerate(annotation['frames']):
+                # Load image to PIL format
+                img = Image.open(BytesIO(base64.b64decode(frame['screenshot'])))
+                # cache frame - FIXME: currently there is no mean to identify the video - same timstamp will overwrite an old frame (hash?)
+                img.save(os.path.join(CACHE_DIR,'{0}.png'.format(frame['timecode'])))
+                if img.mode != 'RGB':
+                    img = img.convert('RGB')
+                hw_tuple = (target_size, target_size)
+                if img.size != hw_tuple:
+                    logger.warn("Scaling image to model size - this should be done in advene!")
+                    img = img.resize(hw_tuple)
+                x = image.img_to_array(img)
+                x = np.expand_dims(x, axis=0)
+                x = preprocess_input(x)
+                batch_x[i] = x[0,:,:,:]
+            preds = model.predict_on_batch(np.asarray(batch_x))
 
-        # decode the results into a list of tuples (class, description, probability)
-        # (one such list for each sample in the batch)
-        decoded = decode_predictions(preds, top=top_n_preds)
-        confidences = dict()
-        for t in itertools.chain.from_iterable(decoded):
-            if t[1] in confidences:
-                confidences[t[1]].append(float(t[2]))
-            else:
-                confidences[t[1]] = [float(t[2])]
+            # decode the results into a list of tuples (class, description, probability)
+            # (one such list for each sample in the batch)
+            decoded = decode_predictions(preds, top=top_n_preds)
+            confidences = dict()
+            for t in itertools.chain.from_iterable(decoded):
+                if t[1] in confidences:
+                    confidences[t[1]].append(float(t[2]))
+                else:
+                    confidences[t[1]] = [float(t[2])]
+            logger.debug(confidences)
+            
+            concepts.extend([
+            {
+                'annotationid': aid,
+                'confidence': max(confidences[l]),
+                #FIXME: set correct timecode - set timecode of frame with max confidence?
+                'timecode': annotation['begin'], #timestamp_in_ms,
+                'label': l,
+                'uri': 'http://concept.org/%s' % l
+            } for l in confidences]
+            )
 
+        logger.debug(concepts)
         s.send_response(200)
         s.send_header("Content-type", "application/json")
         s.end_headers()
@@ -110,19 +148,9 @@ class RESTHandler(http.server.BaseHTTPRequestHandler):
             "status": 200,
             "message": "OK",
             "data": {
-                # FIXME: JSON skeleton only, fix detection iself (cf
-                # dummy_server.py)
-                'media_filename': 'repeat_from_query',
-                'media_uri': 'repeat from query',
-                'concepts': [
-                    {
-                        'annotationid': aid,
-                        'confidence': max(confidences[l]),
-                        'timecode': timestamp_in_ms,
-                        'label': l,
-                        'uri': 'http://concept.org/%s' % l
-                    } for l in confidences
-                ]
+                'media_filename': post_data["media_filename"],
+                'media_uri': post_data["media_uri"],
+                'concepts': concepts
             }
         }, s.wfile)
 
