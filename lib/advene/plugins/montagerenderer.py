0242f6ab445b108cb29b8e5b31c6c74bd2796bd5
Olivier Aubert <contact@olivieraubert.net>
2020-02-19 15:57:02 +0100

    plugins.montagerenderer: reimplement video extraction

--- lib/advene/plugins/montagerenderer.py
+++ lib/advene/plugins/montagerenderer.py
@@ -1,6 +1,6 @@
-#
+#! /usr/bin/env python3
 # Advene: Annotate Digital Videos, Exchange on the NEt
-# Copyright (C) 2008-2017 Olivier Aubert <contact@olivieraubert.net>
+# Copyright (C) 2020 Olivier Aubert <contact@olivieraubert.net>
 #
 # Advene is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,184 +17,152 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 """Montage export module.
+
+This filter exports a list of annotations as a video montage
 """
 
-import os
-import sys
-
-try:
-    import gst
-except ImportError:
-    gst = None
-
-from gettext import gettext as _
+name="Video montage renderer"
+shortname = 'montagerenderer'
 
 import logging
 logger = logging.getLogger(__name__)
 
-name = 'montagerenderer'
+from gettext import gettext as _
 
-def register(controller):
-    if ( gst is not None
-         and gst.element_factory_find('gnlcomposition')
-         and gst.element_factory_find('gnlfilesource')
-         and gst.element_factory_find('theoraenc')
-         and gst.element_factory_find('vorbisenc')
-         and gst.element_factory_find('oggmux') ):
-        controller.register_generic_feature(name, MontageRenderer)
-    else:
-        controller.log(_("Cannot register montage renderer: Gnonlin plugins are not present."))
+import gi
+gi.require_version('GES', '1.0')
+gi.require_version('Gst', '1.0')
+gi.require_version('GstPbutils', '1.0')
+from gi.repository import GLib
+from gi.repository import Gst
+from gi.repository import GstPbutils
+from gi.repository import GES
+Gst.init([])
+GES.init()
 
-if gst is not None:
-    CAPS_VIDEO_STRING = 'video/x-raw-yuv'
-    CAPS_VIDEO = gst.caps_from_string(CAPS_VIDEO_STRING)
-    CAPS_AUDIO_STRING = 'audio/x-raw-int;audio/x-raw-float'
-    CAPS_AUDIO = gst.caps_from_string(CAPS_AUDIO_STRING)
+import advene.core.config as config
+import advene.util.helper as helper
+
+def register(controller=None):
+    controller.register_generic_feature(shortname, MontageRenderer)
+    return True
 
 class MontageRenderer(object):
+    """Video montage exporter.
+    """
+    name = _("Video montage exporter")
+
     def __init__(self, controller, elements=None):
         self.controller = controller
-        # self.elements is a list of annotations
-        if elements is None:
-            elements = []
         self.elements = elements
-        self.encoding_pipe = None
-
-    def render(self, outputfile, progress_callback = None):
-        sourcefile = 'file:///' + self.controller.get_default_media()
-        if not sourcefile:
-            return
-        pipedef = "gnlcomposition name=videocomp caps=%s ! queue ! progressreport name=progress silent=true update-freq=1 ! identity single-segment=true ! ffmpegcolorspace ! videorate ! theoraenc ! multiqueue name=muxqueue ! oggmux name=mux ! filesink name=sink gnlcomposition name=audiocomp caps=%s ! queue ! identity single-segment=true ! audioconvert ! audiorate ! vorbisenc ! muxqueue." % (CAPS_VIDEO_STRING, CAPS_AUDIO_STRING)
-        pipe = gst.parse_launch(pipedef)
-        videocomp = pipe.get_by_name('videocomp')
-        audiocomp = pipe.get_by_name('audiocomp')
-        sink = pipe.get_by_name('sink')
-        bus = pipe.get_bus()
-        bus.enable_sync_message_emission()
-
-        def filesource(a, pos, caps):
-            """Create a filesource.
-            """
-            e = gst.element_factory_make('gnlfilesource')
-            e.set_property("location", sourcefile)
-            e.set_property("caps", caps)
-            e.set_property("start", pos * gst.MSECOND)
-            e.set_property("duration", a.fragment.duration * gst.MSECOND)
-            e.set_property("media-start", a.fragment.begin * gst.MSECOND)
-            e.set_property("media-duration", a.fragment.duration * gst.MSECOND)
-            return e
-
-        pos = 0
-        for a in self.elements:
-            e = filesource(a, pos, CAPS_VIDEO)
-            videocomp.add(e)
-            e = filesource(a, pos, CAPS_AUDIO)
-            audiocomp.add(e)
-            pos += a.fragment.duration
-
-        sink.set_property("location", outputfile)
-
-        def on_bus_message(bus, message):
-            s = message.get_structure()
-            if message.type == gst.MESSAGE_STATE_CHANGED:
-                old, new, pending = message.parse_state_changed()
-                logger.warn("STATE %s %s %s", old.value_nick, new.value_nick, pending.value_nick)
-            elif message.type == gst.MESSAGE_EOS:
-                logger.warn(" EOS")
-                pipe.set_state(gst.STATE_NULL)
-                progress_callback(None)
-            elif s:
-                logger.warn("MSG %s %s", bus.get_name(), s.to_string())
-                if s.get_name() == 'progress' and progress_callback is not None:
-                    progress_callback(s['percent-double'] / 100)
-            return True
-
-        bus.add_signal_watch()
-        bus.connect('message', on_bus_message)
-        pipe.set_state(gst.STATE_PLAYING)
-        self.encoding_pipe = pipe
-        return True
+        self.progress_cb = None
+        self.pipeline = None
 
     def finalize(self):
-        if self.encoding_pipe is not None:
-            self.encoding_pipe.set_state(gst.STATE_NULL)
-            self.encoding_pipe = None
+        if self.pipeline is not None:
+            self.pipeline.set_state(Gst.State.NULL)
+            self.pipeline = None
 
-    # relpath, dump_bin and dump_element implementation based on Daniel Lenski <dlenski@gmail.com>
-    # posted on gst-dev mailing list on 20070913
-    def relpath(self, p1, p2):
-        sep = os.path.sep
+    def bus_message_cb(self, unused_bus, message):
+        if message.type == Gst.MessageType.EOS:
+            logger.warning("End of encoding")
+            self.progress_cb(None)
 
-        # get common prefix (up to a slash)
-        common = os.path.commonprefix((p1, p2))
-        common = common[:common.rfind(sep)]
+    def duration_querier(self):
+        pos = self.pipeline.query_position(Gst.Format.TIME)[1] / self.total_duration
+        if self.progress_cb:
+            self.progress_cb(pos)
+        return True
 
-        # remove common prefix
-        p1 = p1[len(common)+1:]
-        p2 = p2[len(common)+1:]
+    def render(self, filename, progress_callback=None):
+        # Works if source is a type
+        self.progress_cb = progress_callback
 
-        # number of seps in p1 is # of ..'s needed
-        return "../" * p1.count(sep) + p2
+        # FIXME: considering single-video for the moment
+        package = self.elements[0].ownerPackage
+        media_uri = package.getMetaData(config.data.namespace, "media_uri") or package.mediafile
+        media_uri = helper.path2uri(media_uri)
 
-    def dump_bin(self, bin, depth=0, recurse=-1, showcaps=True):
-        return [ l  for e in reversed(list(bin)) for l in self.dump_element(e, depth, recurse - 1) ]
+        logger.warning("Extracting clips from %s", media_uri)
+        asset = GES.UriClipAsset.request_sync(media_uri)
 
-    def dump_element(self, e, depth=0, recurse=-1, showcaps=True):
-        ret=[]
-        indentstr = depth * 8 * ' '
+        timeline = GES.Timeline.new_audio_video()
+        layer = timeline.append_layer()
 
-        # print element path and factory
-        path = e.get_path_string() + (isinstance(e, gst.Bin) and '/' or '')
-        factory = e.get_factory()
-        if factory is not None:
-            ret.append( '%s%s (%s)' % (indentstr, path, factory.get_name()) )
-        else:
-            ret.append( '%s%s (No factory)' % (indentstr, path) )
+        start_on_timeline = 0
 
-        # print info about each pad
-        for p in e.pads():
-            name = p.get_name()
+        self.total_duration = sum(a.fragment.duration * Gst.MSECOND for a in self.elements)
+        clips = []
+        for a in self.elements:
+            start_position_asset = a.fragment.begin * Gst.MSECOND
+            duration = a.fragment.duration * Gst.MSECOND
+            # GES.TrackType.UNKNOWN => add every kind of stream to the timeline
+            clips.append(layer.add_asset(asset, start_on_timeline, start_position_asset,
+                                        duration, GES.TrackType.UNKNOWN))
+            start_on_timeline += duration
 
-            # negotiated capabilities
-            caps = p.get_negotiated_caps()
-            if caps: capsname = caps[0].get_name()
-            elif showcaps: capsname = '; '.join(s.to_string() for s in set(p.get_caps()))
-            else: capsname = None
+        timeline.commit()
 
-            # flags
-            flags = []
-            if not p.is_active(): flags.append('INACTIVE')
-            if p.is_blocked(): flags.append('BLOCKED')
+        # Build the encoding pipeline
+        pipeline = GES.Pipeline()
+        pipeline.set_timeline(timeline)
 
-            # direction
-            direc = (p.get_direction() is gst.PAD_SRC) and "=>" or "<="
+        container_profile = \
+            GstPbutils.EncodingContainerProfile.new("montage-profile",
+                                                    "Pitivi encoding profile",
+                                                    Gst.Caps("video/webm"),
+                                                    None)
+        video_profile = GstPbutils.EncodingVideoProfile.new(Gst.Caps("video/x-vp8"),
+                                                            None,
+                                                            Gst.Caps("video/x-raw"),
+                                                            0)
 
-            # peer
-            peer = p.get_peer()
-            if peer: peerpath = self.relpath(path, peer.get_path_string())
-            else: peerpath = None
+        container_profile.add_profile(video_profile)
 
-            # ghost target
-            if isinstance(p, gst.GhostPad):
-                target = p.get_target()
-                if target: ghostpath = target.get_path_string()
-                else: ghostpath = None
-            else:
-                ghostpath = None
+        audio_profile = GstPbutils.EncodingAudioProfile.new(Gst.Caps("audio/x-vorbis"),
+                                                            None,
+                                                            Gst.Caps("audio/x-raw"),
+                                                            0)
 
-            line=[ indentstr, "    " ]
-            if flags: line.append( ','.join(flags) )
-            line.append(".%s" % name)
-            if capsname: line.append( '[%s]' % capsname )
-            if ghostpath: line.append( "ghosts %s" % self.relpath(path, ghostpath) )
-            line.append( "%s %s" % (direc, peerpath) )
+        container_profile.add_profile(audio_profile)
 
-            #if peerpath and peerpath.find('proxy')!=-1: print peer
-            ret.append( ''.join(line) )
-        if recurse and isinstance(e, gst.Bin):
-            ret.extend( self.dump_bin(e, depth+1, recurse) )
-        return ret
+        pipeline.set_render_settings(helper.path2uri(filename), container_profile)
+        pipeline.set_mode(GES.PipelineFlags.RENDER)
 
-    def str_element(self, element):
-        return "\n".join(self.dump_element(element))
+        self.pipeline = pipeline
+        logger.warning("Starting encoding")
+        self.pipeline.set_state(Gst.State.PLAYING)
 
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.bus_message_cb)
+        GLib.timeout_add(300, self.duration_querier)
+
+if __name__ == '__main__':
+    import sys
+    logging.basicConfig(level=logging.DEBUG)
+    USAGE = "%prog package_file type_name"
+    if sys.argv[2:]:
+        packagename = sys.argv[1]
+        atype = sys.argv[2]
+    else:
+        logger.warning(USAGE)
+        sys.exit(1)
+
+    from advene.model.package import Package
+
+    logger.warning("Extracting from %s - type %s", packagename, atype)
+    p = Package(packagename)
+    at = p.get_element_by_id(atype)
+
+    r = MontageRenderer(None, sorted(at.annotations))
+
+    mainloop = GLib.MainLoop()
+    def pg(value):
+        if value is None:
+            mainloop.quit()
+            return
+        logging.warning("Progress %d", value)
+
+    r.render('/tmp/montage.webm', pg)
+    mainloop.run()
