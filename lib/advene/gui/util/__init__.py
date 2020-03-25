157f1e5c7e46418b7b13160af88c9b325513f20b
Olivier Aubert <contact@olivieraubert.net>
2020-03-25 19:00:16 +0100

    gui.util: require appropriate versions of Gdk/Gtk

--- lib/advene/gui/util/__init__.py
+++ lib/advene/gui/util/__init__.py
@@ -27,6 +27,8 @@ import advene.core.config as config
 
 import cairo
 import gi
+gi.require_version('Gdk', '3.0')
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
 from gi.repository import Gtk
