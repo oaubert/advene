0b91b1588edfaae1c4b2aaf5288a9f1c85f107ee
Olivier Aubert <contact@olivieraubert.net>
2020-03-25 19:10:43 +0100

    plugins.vian_import: improve VIAN import (thanks to Gaudenz Halter's contributions)

--- lib/advene/plugins/vian_import.py
+++ lib/advene/plugins/vian_import.py
@@ -42,7 +42,6 @@ class VIANImporter(GenericImporter):
 
     def can_handle(fname):
         """Return a score between 0 and 100.
-
         100 is for the best match (specific extension), 0 is for no match at all.
         """
         ext = os.path.splitext(fname)[1]
@@ -54,18 +53,22 @@ class VIANImporter(GenericImporter):
     def process_file(self, filename, dest=None):
         with open(filename, 'r') as f:
             vian = json.load(f)
+        hdfname = os.path.join(os.path.dirname(filename), 'data', 'analyses.hdf5')
+        if not os.path.exists(hdfname):
+            hdfname = None
         p, at = self.init_package(filename=dest)
         p.setMetaData(config.data.namespace_prefix['dc'],
                       'description',
                       _("Converted from %s") % filename)
-        self.convert(self.iterator(vian))
+        self.convert(self.iterator(vian, hdfname))
         self.progress(1.0)
         return self.package
 
-    def iterator(self, vian):
+    def iterator(self, vian, hdf5_path = None):
         """Iterate through the loaded JSON
         """
         progress=0.01
+
         self.progress(progress, "Starting conversion")
 
         self.package.setMedia(vian['movie_descriptor']['movie_path'])
@@ -78,10 +81,12 @@ class VIANImporter(GenericImporter):
         if ntypes > 0:
             incr = 1 / ntypes
 
-        segment_cache = {}
+        annotation_cache = {}
+
         # Map segments to annotations and keep a dict of segments
         for segmentation in vian['segmentation']:
             progress += incr
+
             self.progress(progress, f"Converting segmentation {segmentation['name']}")
             new_atype = self.ensure_new_type(
                 segmentation['unique_id'],
@@ -100,29 +105,173 @@ class VIANImporter(GenericImporter):
                     # FIXME: we lose the segment['name'] here
                     "content": segment['annotation_body'][0]['content'],
                 }
-                segment_cache[an['id']] = an
+                annotation_cache[an['id']] = an
                 yield an
+
+        # We also have to collect the screenshots to map to the classification
+        new_atype = self.ensure_new_type(
+            'screenshot',
+            title='Screenshot')
+        for scr in vian['screenshots']:
+            annotation_cache[scr['unique_id']] = {
+                "type": new_atype,
+                "begin": scr['start_ms'],
+                "end": scr['end_ms'],
+                "id": scr['unique_id'],
+                "content": scr['name'],
+            }
+
+        # VIAN Keywords are a composite of VocabularyWord and ClassificationObject
+        # We first have to collect all vocabularies from the project, before we can unpack the experiment.
+        # These are stored globally in the project root, and referenced in the experiments
+        vocabulary_words = {}
+        for voc in vian['vocabularies']:
+            for word in voc['words']:
+                advene_word = dict(
+                    id = word['unique_id'],
+                    vocabulary = voc['name'],
+                    word = word['name']
+                )
+                vocabulary_words[word['unique_id']] = advene_word
+
+        # To resolve the classification within an experiment we have
+        # to collect the classification objects in a map
+
+        # Because the analyses currently reference the
+        # ClassificationObject and not the semantic segmentation
+        # labels, we have to keep them globally, this will likely
+        # change in VIAN in the future.
+        all_classification_objects = {}
 
         # Map classification results to annotations
         for expe in vian['experiments']:
             progress += incr
+
             self.progress(progress, f"Converting experiment {expe['name']}")
             new_atype = self.ensure_new_type(
                 expe['unique_id'],
                 title = expe['name'])
             new_atype.mimetype = 'text/x-advene-keyword-list'
 
+            # Again, VIAN keywords are composites of a VocabularyWord and a ClassificationObject
+            # We now collect the classification objects from this experiment, an store them globally
+            # dereferencing them in the later analyses
+
+            # The keywords for each ClassificationObject are store within it.
+            experiment_keywords = {}
+
+            for cl_obj in expe['classification_objects']:
+
+                # First extract the classification object
+                all_classification_objects[cl_obj['unique_id']] = dict(
+                    id = cl_obj['unique_id'],
+                    name=cl_obj['name'],
+                    semantic_segmentation = cl_obj['semantic_segmentation_labels'],
+                )
+
+                # Then we assemble all keywords which are stored within the classification object:
+                for kwd in cl_obj['unique_keywords']:
+                    experiment_keywords[kwd['unique_id']] = dict(
+                        id=kwd['unique_id'],
+                        voc_word = vocabulary_words[kwd['word_obj']],
+                        classification_obj = all_classification_objects[cl_obj['unique_id']]
+                    )
+
             for result in expe['classification_results']:
-                an = segment_cache.get(result['target'])
-                if an is None:
+                target = annotation_cache.get(result['target'])
+                keyword = experiment_keywords.get(result['keyword'])
+
+                if target is None:
                     logger.warn("Cannot find segment for %s", result['target'])
+                elif keyword is None:
+                    logger.warn("Cannot find keyword for %s", result['keyword'])
                 else:
                     yield {
                         "type": new_atype,
-                        "begin": an['begin'],
-                        "end": an['end'],
+                        "begin": target['begin'],
+                        "end": target['end'],
                         # FIXME: how to dereference keyword information?
-                        "content": result['keyword'],
+                        "content": keyword,
                     }
+
+        # Extract the HDF5-stored analyses results.
+        # I just import them by example including the values from the hdf5 file, so you know how to access them
+        if hdf5_path is not None:
+            import h5py
+            import numpy as np
+            hdf5_file = h5py.File(hdf5_path, mode="r")
+            # data tables: hdf5_file.keys()
+            for an in vian['analyses']:
+                # If the analysis is not one of the following serialization types, it is probably not important for Advene
+                # there is also the ColorimetryAnalysis, which contains a continous dataset, we can discuss this later
+                if an['vian_serialization_type'] not in ['SemanticSegmentationAnalysisContainer', 'IAnalysisJobAnalysis']:
+                    continue
+
+                # Find the annotation which this analysis has been computed on
+                target = annotation_cache.get(an['container'])
+                if target is None:
+                    logger.warn("Cannot find target for %s", an['container'])
+                    continue
+
+                hdf5_dataset = an['hdf5_location']['hdf5_dataset']
+                hdf5_index = an['hdf5_location']['hdf5_index']
+
+                analysis = dict(
+                    id = an['unique_id'],
+                    begin = target['begin'],
+                    end = target['end'],
+                    content = an['vian_analysis_type'],
+                    vian_analysis_type = an['vian_analysis_type'],
+                    hdf5_dataset = hdf5_dataset,
+                    hdf5_index = hdf5_index
+                )
+
+                # Semantic segmentations have the vian_analysis_type SemanticSegmentationAnalysis
+                if an['vian_analysis_type'] == "SemanticSegmentationAnalysis":
+                    # This is the same as in ClassificationObject.semantic_segmentation_labels.model
+                    analysis['semantic_segmentation_dataset'] = an['dataset']
+
+                    # A mask of labels referencing corresponding to
+                    # ClassificationObject.semantic_segmentation_labels.labels.label
+                    analysis['mask'] = hdf5_file[hdf5_dataset][hdf5_index]
+
+                else:
+                    # if the classification object is not -1, a semantic segmentation has been used with this
+                    # analysis.
+                    cl_obj = an['classification_obj']
+                    if cl_obj != -1:
+                        cl_obj = all_classification_objects.get(cl_obj)
+                    else:
+                        cl_obj = None
+                    analysis['classification_object'] = cl_obj
+
+                    # Extract the color features
+                    if an['vian_analysis_type'] == "ColorFeatureAnalysis":
+                        # See feature vector memory layout here
+                        # https://www.vian.app/static/manual/dev_guide/analyses/color_features.html#module-core.analysis.color_feature_extractor
+
+                        analysis['lab'] = hdf5_file[hdf5_dataset][hdf5_index][:3]
+                        analysis['rgb'] = hdf5_file[hdf5_dataset][hdf5_index][3:6][::-1]
+
+                    elif an['vian_analysis_type'] == "ColorPaletteAnalysis":
+                        # See feature vector memory layout here
+                        # https://www.vian.app/static/manual/dev_guide/analyses/color_features.html#module-core.analysis.palette_analysis
+                        merge_depth = 6
+                        total_palette = hdf5_file[hdf5_dataset][hdf5_index]
+                        merge_depth_indices = np.where(total_palette[:, 1] == merge_depth)[0]
+                        total_num_pixel = np.sum(total_palette[merge_depth_indices, 5])
+                        analysis['palette'] = []
+                        for j in merge_depth_indices.tolist():
+                            analysis['palette'].append(dict(
+                                color_rgb = total_palette[j,2:5][::-1].tolist(),
+                                relative_size = (total_palette[j,5].astype(np.float32) / total_num_pixel).tolist()
+                            ))
+
+                    elif an['vian_analysis_type'] == "ColorHistogramAnalysis":
+                        # See feature vector memory layout here
+                        # https://www.vian.app/static/manual/dev_guide/analyses/color_features.html#module-core.analysis.histogram_analysis
+                        analysis['color_histogram'] = hdf5_file[hdf5_dataset][hdf5_index]
+
+                yield analysis
 
         self.progress(1.0)
