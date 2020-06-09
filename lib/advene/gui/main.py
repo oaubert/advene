3a07b88f802c31e053dbfcc96ce5f40bda3ad6a2
Olivier Aubert <contact@olivieraubert.net>
2020-06-09 19:15:00 +0200

    gui.main: fix typo

--- lib/advene/gui/main.py
+++ lib/advene/gui/main.py
@@ -1159,7 +1159,7 @@ class AdveneGUI:
         self.gui.application_space.add(self.visualisationwidget)
 
         def media_changed(context, parameters):
-            if config.data.preferences['player-autostart'] and not 'record' in self.controller.player.player_capabiolities:
+            if config.data.preferences['player-autostart'] and not 'record' in self.controller.player.player_capabilities:
                 self.controller.queue_action(self.controller.update_status, "start")
                 self.controller.queue_action(self.controller.update_status, "pause")
 
