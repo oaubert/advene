8eae7babd3033ae20c0ea15db66456c1320905bc
Olivier Aubert <contact@olivieraubert.net>
2023-05-30 16:37:08 +0200

    main: implement wrapper to make sure logging is initialized

--- lib/advene/main.py
+++ lib/advene/main.py
@@ -33,6 +33,41 @@ import time
 
 import advene.core.config as config
 
+class StreamToLogger:
+    """Fake file-like stream object that redirects writes to a logger instance.
+
+    Adapted from https://stackoverflow.com/a/36296215
+    """
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        temp_linebuf = self.linebuf + buf
+        self.linebuf = ''
+        for line in temp_linebuf.splitlines(True):
+            # From the io.TextIOWrapper docs:
+            #   On output, if newline is None, any '\n' characters written
+            #   are translated to the system default line separator.
+            # By default sys.stdout.write() expects '\n' newlines and then
+            # translates them so this is still cross platform.
+            if line[-1] == '\n':
+                self.logger.log(self.log_level, line.rstrip())
+            else:
+                self.linebuf += line
+
+    def flush(self):
+        if self.linebuf != '':
+            self.logger.log(self.log_level, self.linebuf.rstrip())
+        self.linebuf = ''
+
+# This may happen on Windows when launched from GUI
+if sys.stdout is None:
+    sys.stdout = StreamToLogger(logging.getLogger('STDOUT'), logging.INFO)
+if sys.stderr is None:
+    sys.stderr = StreamToLogger(logging.getLogger('STDERR'), logging.ERROR)
+
 def main(app_dir=None):
 
     if app_dir is not None:
