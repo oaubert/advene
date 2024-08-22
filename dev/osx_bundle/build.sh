167d1fe2632fac6e04810b31f1cf1250294a32b6
Olivier Aubert <contact@olivieraubert.net>
2024-08-22 20:48:18 +0200

    Cherrypy is installed through jhbuild

--- dev/osx_bundle/build.sh
+++ dev/osx_bundle/build.sh
@@ -9,7 +9,3 @@ cargo -V || (echo "Requires rust"; exit 1)
 
 jhbuild build meta-bootstrap
 jhbuild build advene
-# Add python package through pip, since it handles dependencies automatically
-echo python3 -m pip install cherrypy | jhbuild shell
-echo python3 -m pip install Pillow | jhbuild shell
-echo python3 -m pip install rdflib | jhbuild shell
