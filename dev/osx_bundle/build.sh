4f3cf6420e25825288865a292534e6a3e4756226
Olivier Aubert <contact@olivieraubert.net>
2024-08-29 16:05:14 +0200

    OSX Build advene

--- dev/osx_bundle/build.sh
+++ dev/osx_bundle/build.sh
@@ -8,15 +8,4 @@ source env.sh
 cargo -V || (echo "Requires rust"; exit 1)
 
 jhbuild --no-interact build meta-bootstrap
-#jhbuild build advene
-jhbuild --no-interact build pulseaudio
-
-# Ref dir:
-#  /Users/runner/work/advene/advene/dev/osx_bundle/_home/jhbuild_checkoutroot/pulseaudio-v17.0/
-LOGDIR=/Users/runner/work/advene/advene/dev/osx_bundle/_home/.cache/jhbuild/build/pulseaudio-v17.0/meson-logs/
-ls -l $LOGDIR
-echo "################################################################################# meson-log.txt"
-[ -f $LOGDIR/meson-log.txt ] && cat $LOGDIR/meson-log.txt
-echo "################################################################################# install-log.txt"
-[ -f $LOGDIR/install-log.txt ] && cat $LOGDIR/install-log.txt
-echo "#################################################################################"
+jhbuild --no-interact build advene
