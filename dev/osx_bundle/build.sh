740f8b73094a25bf37f8fbd2ba4d3f9331e31d56
Olivier Aubert <contact@olivieraubert.net>
2024-08-29 11:49:20 +0200

    OSX Try to display pulseaudio logs

--- dev/osx_bundle/build.sh
+++ dev/osx_bundle/build.sh
@@ -10,3 +10,13 @@ cargo -V || (echo "Requires rust"; exit 1)
 jhbuild build meta-bootstrap
 #jhbuild build advene
 jhbuild build pulseaudio
+
+# Ref dir:
+#  /Users/runner/work/advene/advene/dev/osx_bundle/_home/jhbuild_checkoutroot/pulseaudio-v17.0/
+LOGDIR=/Users/runner/work/advene/advene/dev/osx_bundle/_home/.cache/jhbuild/build/pulseaudio-v17.0/meson-logs/
+ls -l $LOGDIR
+echo "################################################################################# meson-log.txt"
+[ -f $LOGDIR/meson-log.txt ] && cat $LOGDIR/meson-log.txt
+echo "################################################################################# install-log.txt"
+[ -f $LOGDIR/install-log.txt ] && cat $LOGDIR/install-log.txt
+echo "#################################################################################"
