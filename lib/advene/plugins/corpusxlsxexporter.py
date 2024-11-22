90eb8d11039b97273dcb1bc7d2d885aa9f17aee2
Olivier Aubert <contact@olivieraubert.net>
2024-11-22 16:25:43 +0100

    plugins.corpusxlsxexporter: add more export options

--- lib/advene/plugins/corpusxlsxexporter.py
+++ lib/advene/plugins/corpusxlsxexporter.py
@@ -37,7 +37,7 @@ import advene.util.helper as helper
 
 try:
     import openpyxl
-    from openpyxl.utils.cell import get_column_letter
+    from openpyxl.styles import Font
 except ImportError:
     openpyxl = None
 
@@ -55,80 +55,137 @@ class CorpusXlsxExporter(GenericExporter):
 
     def __init__(self, controller=None, source=None, callback=None):
         super().__init__(controller, source, callback)
-        self.title = False
+        self.sheet_global = True
+        self.sheet_by_package = False
+        self.sheet_by_annotationtype = True
+        self.sheet_by_relationtype = False
         package_count = len(controller.global_package.package_list)
         self.all = controller.global_package
         self.optionparser.description = _(f"Export {package_count} packages as Xlsx file.")
-        self.optionparser.add_option("-t", "--title",
-                                     action="store_true", dest="title", default=self.title,
-                                     help=_("Use annotation type titles instead of ids"))
+        self.optionparser.add_option("-r", "--relation-types",
+                                     action="store_true", dest="sheet_by_relationtype", default=self.sheet_by_relationtype,
+                                     help=_("Create 1 tab per relation-type"))
+        self.optionparser.add_option("-a", "--annotation-types",
+                                     action="store_true", dest="sheet_by_annotationtype", default=self.sheet_by_annotationtype,
+                                     help=_("Create 1 tab per annotation-type"))
+        self.optionparser.add_option("-p", "--packages",
+                                     action="store_true", dest="sheet_by_package", default=self.sheet_by_package,
+                                     help=_("Create 1 tab per package"))
+        self.optionparser.add_option("-g", "--global",
+                                     action="store_true", dest="sheet_global", default=self.sheet_global,
+                                     help=_("Create 1 global sheet with all annotation"))
 
     @classmethod
     def is_valid_for(cls, expr):
         return expr == 'package'
 
+    def fit_column_width(self, sheet):
+        # Iterate over all columns and adjust their widths
+        for column in sheet.columns:
+            column_letter = column[0].column_letter
+            max_length = max(len(str(cell.value))
+                             for cell in column)
+            sheet.column_dimensions[column_letter].width = min(50,
+                                                               max_length + 2)
+
     def export(self, filename=None):
         # Export a corpus as a Xlsx file
-        # One sheet per package, then one annotation per line
-        # One sheet per annotation type, one annotation per line
 
         book = openpyxl.Workbook()
 
         # Remove default worksheet
         book.remove(book.worksheets[0])
 
-        # Check if titles are different
-        titles = [ p.title for p in self.all.package_list ]
-        use_package_titles = True
-
-        if len(set(titles)) != len(titles):
-            logger.warning(_("Some package titles are identical, use package filename as sheet name"))
-            use_package_titles = False
-
         # Add summary worksheet
         sheet = book.create_sheet('Summary')
-        sheet.append([ "alias", "title", "media", "annotation count", "annotation type count", "relation type count" ])
+        header = [
+            "alias",
+            "title",
+            "media",
+            "annotation count",
+            "annotation type count",
+            "relation type count",
+            "media duration"
+        ]
+        sheet.append(header)
         for alias, p in self.controller.packages.items():
+            if alias == 'advene':
+                continue
+            # Store alias as p attribute, for reference in next steps
+            p.alias = alias
             media_uri = os.path.basename(p.getMetaData(config.data.namespace, "media_uri") or "unknown")
-            sheet.append([ alias,
-                           p.title,
-                           media_uri,
-                           len(p.annotations),
-                           len(p.annotationTypes),
-                           len(p.relationTypes) ])
+            sheet.append([
+                alias,
+                p.title,
+                media_uri,
+                len(p.annotations),
+                len(p.annotationTypes),
+                len(p.relationTypes),
+                p.getMetaData(config.data.namespace, "duration") or ""
+            ])
+            for row in sheet.iter_rows(min_row=1, max_row=1, max_col=1 + len(header)):
+                for cell in row:
+                    cell.font = Font(bold=True)
+
+        self.fit_column_width(sheet)
 
         def save_annotations(ws, annotations):
-            header = [ "id", "text", "begin", "begin_ms", "end", "end_ms", "type_id", "type_title" ]
+            header = [ "text", "begin", "begin_ms", "end", "end_ms", "type_id", "type_title", "id", "package" ]
             ws.append(header)
+            for row in sheet.iter_rows(min_row=1, max_row=1, max_col=1 + len(header)):
+                for cell in row:
+                    cell.font = Font(bold=True)
             for a in annotations:
                 ws.append([
-                    a.id,
                     self.controller.get_title(a),
                     helper.format_time_reference(a.fragment.begin),
                     a.fragment.begin,
                     helper.format_time_reference(a.fragment.end),
                     a.fragment.end,
                     a.type.id,
-                    self.controller.get_title(a.type)
+                    self.controller.get_title(a.type),
+                    a.id,
+                    a.ownerPackage.alias
                 ])
-            # Set dimensions
-            for i in range(len(header)):
-                 column_letter = get_column_letter(i + 1)
-                 ws.column_dimensions[column_letter].width = 10
+            self.fit_column_width(sheet)
 
         # Add global sheet
-        sheet = book.create_sheet('Global')
-        save_annotations(sheet, sorted(self.controller.global_package.annotations))
+        if self.sheet_global:
+            sheet = book.create_sheet('Global')
+            save_annotations(sheet, sorted(self.controller.global_package.annotations))
 
         # Create 1 sheet per package
-        for alias, p in self.controller.packages.items():
-            if alias == 'advene':
-                continue
-            sheet = book.create_sheet(alias)
+        if self.sheet_by_package:
+            for alias, p in self.controller.packages.items():
+                if alias == 'advene':
+                    continue
+                sheet = book.create_sheet(alias)
 
-            media_uri = p.getMetaData(config.data.namespace, "media_uri") or "unknown"
+                media_uri = p.getMetaData(config.data.namespace, "media_uri") or "unknown"
 
-            save_annotations(sheet, p.annotations)
+                save_annotations(sheet, p.annotations)
+
+        # Create 1 sheet per annotation type (label)
+        if self.sheet_by_annotationtype:
+            at_titles = set(at.title for at in self.controller.global_package.annotationTypes)
+            for at_title in sorted(at_titles):
+                sheet = book.create_sheet(at_title)
+                save_annotations(sheet, (a
+                                         for alias, p in self.controller.packages.items()
+                                         if alias != 'advene'
+                                         for a in sorted(p.annotations)
+                                         if a.type.title == at_title))
+
+        # Create 1 sheet per relation type (label)
+        if self.sheet_by_relationtype:
+            rt_titles = set(rt.title for rt in self.controller.global_package.annotationTypes)
+            for rt_title in sorted(rt_titles):
+                sheet = book.create_sheet(rt_title)
+                save_annotations(sheet, (a
+                                         for alias, p in self.controller.packages.items()
+                                         if alias != 'advene'
+                                         for a in sorted(p.annotations)
+                                         if rt_title in (r.type.title for r in a.relations)))
 
         book.save(filename)
         return filename
