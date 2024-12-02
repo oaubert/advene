d2b54a48718a7fb4bd166af508655e648e418d38
Olivier Aubert <contact@olivieraubert.net>
2024-12-02 18:02:02 +0100

    gui.views.corpusstatistics: add Relation summary view

--- lib/advene/gui/views/corpusstatistics.py
+++ lib/advene/gui/views/corpusstatistics.py
@@ -27,6 +27,7 @@ from gi.repository import GObject
 from gi.repository import Gtk
 
 from advene.gui.views import AdhocView
+from advene.gui.views.table import AnnotationTable
 
 name = "Corpus statistics"
 
@@ -53,6 +54,29 @@ class CorpusStatistics(AdhocView):
         return dict( (k, v)
                       for (k, v) in self.controller.packages.items()
                       if k != "advene" )
+
+    def relationtypes_liststore(self):
+        model = dict(element=GObject.TYPE_PYOBJECT,
+                     package_alias=GObject.TYPE_STRING,
+                     id=GObject.TYPE_STRING,
+                     title=GObject.TYPE_STRING,
+                     relation_count=GObject.TYPE_INT,
+                     annotation_count=GObject.TYPE_INT)
+
+        # Store reference to the element (at), source package alias, string representation (title)
+        store = Gtk.ListStore(*model.values())
+        store.column = dict((alias, index) for (index, alias) in enumerate(model.keys()))
+
+        for alias, package in self.packages.items():
+            for rt in package.relationTypes:
+                store.append(row=[ rt,
+                                   alias,
+                                   rt.id,
+                                   self.controller.get_title(rt),
+                                   len(rt.relations),
+                                   len(rt.annotations)
+                                  ])
+        return store
 
     def annotationtypes_liststore(self):
         model = dict(element=GObject.TYPE_PYOBJECT,
@@ -86,6 +110,61 @@ class CorpusStatistics(AdhocView):
         <b>{len(self.packages)} loaded packages</b>
 {packages_info}
         """)
+
+    def build_relationtype_table(self, callback=None):
+        store = self.relationtypes_liststore()
+        tree_view = Gtk.TreeView(store)
+        select = tree_view.get_selection()
+        select.set_mode(Gtk.SelectionMode.MULTIPLE)
+        tree_view.set_enable_search(False)
+
+        columns = {}
+        for (name, label, col) in (
+                ('package', _("Package"), store.column['package_alias']),
+                ('title', _("Relation"), store.column['title']),
+                ('annotations', _("Annotations"), store.column['annotation_count']) ):
+            columns[name] = Gtk.TreeViewColumn(label,
+                Gtk.CellRendererText(),
+                text=col)
+            columns[name].set_reorderable(True)
+            columns[name].set_sort_column_id(col)
+            tree_view.append_column(columns[name])
+
+        # Resizable columns: title, type
+        for name in ('title', 'package'):
+            columns[name].set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+            columns[name].set_resizable(True)
+            columns[name].set_min_width(40)
+        columns['title'].set_expand(True)
+
+        sw = Gtk.ScrolledWindow()
+        sw.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        sw.add(tree_view)
+
+        sw.treeview = tree_view
+
+        def row_activated_cb(widget, path, view_column):
+            if callback:
+                callback(store[path][store.column['element']])
+            return True
+        tree_view.connect('row-activated', row_activated_cb)
+
+        return sw
+
+    def build_relationtype_explorer(self):
+        """2-paned relationtype/relation table explorer
+        """
+        explorer = Gtk.Paned.new(Gtk.Orientation.HORIZONTAL)
+        explorer.set_name('relationtype_explorer')
+        annotation_table = AnnotationTable(controller=self.controller, elements=[])
+        def selected_type(rt):
+            annotation_table.set_elements(rt.annotations)
+            return True
+        relationtype_table = self.build_relationtype_table(selected_type)
+        explorer.add1(relationtype_table)
+        explorer.add2(annotation_table.widget)
+        explorer.set_position(400)
+        return explorer
 
     def build_annotation_type_table(self):
         tree_view = Gtk.TreeView(self.annotationtypes_liststore())
@@ -150,11 +229,12 @@ class CorpusStatistics(AdhocView):
         package_count = len(self.packages)
         mainbox.pack_start(Gtk.Label(_(f"Corpus analysis - {package_count} packages")), False, False, 0)
 
-        self.notebook=Gtk.Notebook()
+        self.notebook = Gtk.Notebook()
         self.notebook.set_tab_pos(Gtk.PositionType.TOP)
 
         mainbox.add(self.notebook)
         self.add_page(_("Summary"), self.build_summary())
-        self.add_page(_("Table"), self.build_annotation_type_table())
+        self.add_page(_("Annotations"), self.build_annotation_type_table())
+        self.add_page(_("Relations"), self.build_relationtype_explorer())
 
         return mainbox
